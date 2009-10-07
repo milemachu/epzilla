de7b48fcd44475ae41aae41ba122e91acc6f8b5e
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2009-10-07 10:38:25 +0000

    New Solution structure created

---
+++ trunk/simulation/epZillaSolution/epZillaParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace epZillaParser
+{
+    class Program
+    {
+        static void Main ( string [ ] args )
+        {
+            Console.Write ( "Hello world... This is epZilla" );
+            Console.Read ( );
+        }
+    }
+}
