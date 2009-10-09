e005067ee4a8cf8651baf127d432d4d7e25081b9
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2009-10-09 08:26:06 +0000

    epZilla Shared project added and common interfaces created

---
+++ simulation/epZillaSolution/epZillaShared/IEvent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace epZillaShared
+{
+   public interface IEvent
+    {
+        string ClientId
+        {
+            get;
+            set;
+        }
+        string ClientName
+        {
+            get;
+            set;
+        }
+        string DataItem
+        {
+            get;
+            set;
+        }
+        string EventId
+        {
+            get;
+            set;
+        }
+        DateTime RecievedTime
+        {
+            get;
+            set;
+        }
+        string SourceId
+        {
+            get;
+            set;
+        }
+        string SourceName
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ simulation/epZillaSolution/epZillaShared/ITrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace epZillaShared
+{
+  public  interface ITrigger
+    {
+        DateTime RecievedTime
+        {
+            get;
+            set;
+        }
+        string TriggerData
+        {
+            get;
+            set;
+        }
+        string TriggerId
+        {
+            get;
+            set;
+        }
+        string TriggerSource
+        {
+            get;
+            set;
+        }
+    }
+}
