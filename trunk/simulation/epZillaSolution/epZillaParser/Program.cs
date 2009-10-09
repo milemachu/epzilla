09a7b596e5f5d0e40c49ecf032d660e7d46e5f0f
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2009-10-09 07:19:15 +0000

    epZilla processing engine project coding started.

--- trunk/simulation/epZillaSolution/epZillaParser/Program.cs
+++ trunk/simulation/epZillaSolution/epZillaParser/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using epZillaEventProcessor.EventProcessorEngine;
+
 namespace epZillaParser
 {
     class Program
@@ -10,7 +12,10 @@ namespace epZillaParser
         static void Main ( string [ ] args )
         {
             Console.WriteLine ( "Hello world... This is epZilla" );
-            Console.WriteLine ( "Created by Martin" );
+            Console.WriteLine ( "Created by project epZilla group from UoM" );
+
+            Engine engine = new Engine ( );
+
             Console.Read ( );
         }
     }
