6eebd608ab367f5485e3f2a46cf3793deafe6856
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2009-10-09 07:20:59 +0000

    EventProcessorEngine project codes added

---
+++ trunk/simulation/epZillaSolution/epZillaEventProcessor/EventProcessorEngine/Engine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using epZillaEventProcessor.TriggerBase;
+using epZillaEventProcessor.EventProcessorInfrastructure;
+
+namespace epZillaEventProcessor.EventProcessorEngine
+{
+    /// <summary>
+    /// This is the event Processing Engine.
+    /// This is singke threaded and can process only single dosument at a time.
+    /// Events can not be queued while processing events. 
+    /// Trigger can be added if the processing is not happenning.  
+    /// </summary>
+    public class Engine
+    {
+        private List<Trigger> triggerBase;
+        private List<Event> eventStream;
+        private bool isProcessing;
+        private bool isUpdatingTriggerBase;
+        private bool isUpdatingEventStream;
+
+       
+
+        public Engine ( )
+        {
+            triggerBase = new List<Trigger> ( );
+            eventStream = new List<Event> ( );
+
+            isProcessing = false;
+            isUpdatingTriggerBase = false;
+            isUpdatingEventStream = false;
+            
+            //TEST
+            Console.Write ( "Event Processing Engine Started & Initialized." );
+        }
+
+        public void insertTrigger ( Trigger newTigger )
+        {
+            if ( !isProcessing && newTigger!=null)
+            {
+                isUpdatingTriggerBase = true;
+                triggerBase.Add ( newTigger );
+                isUpdatingTriggerBase = false;
+            }
+        }
+
+        public bool removeTrigger ( Trigger trigger )
+        {
+            if ( !isProcessing && trigger != null )
+            {
+                isUpdatingTriggerBase = true;
+                triggerBase.Remove ( trigger );
+                isUpdatingTriggerBase = false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void insertEvent ( Event newEvent )
+        {
+            if ( !isProcessing && newEvent != null )
+            {
+                isUpdatingEventStream = true;
+                eventStream.Add ( newEvent );
+                isUpdatingEventStream = false;
+            } 
+        }
+
+        public bool removeEvent ( Event oldEvent )
+        {
+            if ( !isProcessing && oldEvent != null )
+            {
+                isUpdatingEventStream = true;
+                eventStream.Remove ( oldEvent );
+                isUpdatingEventStream = false;
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ trunk/simulation/epZillaSolution/epZillaEventProcessor/TriggerBase/Trigger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace epZillaEventProcessor.TriggerBase
+{
+  public  class Trigger
+    {
+        private string triggerId;
+        private string triggerData;
+        private string triggerSource;
+        private DateTime recievedTime;
+        
+
+        public string TriggerData
+        {
+            get
+            {
+                return triggerData;
+            }
+            set
+            {
+                triggerData = value;
+            }
+        }
+        
+
+        public string TriggerSource
+        {
+            get
+            {
+                return triggerSource;
+            }
+            set
+            {
+                triggerSource = value;
+            }
+        }
+        
+
+        public DateTime RecievedTime1
+        {
+            get
+            {
+                return recievedTime;
+            }
+            set
+            {
+                recievedTime = value;
+            }
+        }
+
+        public DateTime RecievedTime
+        {
+            get
+            {
+                return recievedTime;
+            }
+            set
+            {
+                recievedTime = value;
+            }
+        }
+
+
+        public string TriggerId
+        {
+            get
+            {
+                return triggerId;
+            }
+            set
+            {
+                triggerId = value;
+            }
+        }
+
+    }
+}
