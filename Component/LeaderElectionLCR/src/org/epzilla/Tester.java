20716f27543c0f307a5f062cba1c9d881e475736
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2009-12-11 10:43:38 +0000

    Changed for local tests

--- Component/LeaderElectionLCR/src/org/epzilla/Tester.java
+++ Component/LeaderElectionLCR/src/org/epzilla/Tester.java
@@ -7,24 +7,24 @@ public class Tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		MessageListner ml=new MessageListner();
-//		Thread t1=new Thread(ml);
-//		
-//	
-//	t1.start();
+		MessageListner ml=new MessageListner();
+		Thread t1=new Thread(ml);
+		
 	
-	MessageSender ms=new MessageSender("10.8.102.31");
+	t1.start();
+	
+	MessageSender ms=new MessageSender("127.0.0.1");
 
 
 	Thread t2=new Thread(ms);
 	t2.start();
 	
 	
-	MessageSender ms1=new MessageSender("10.8.102.34");
-
-
-	Thread t3=new Thread(ms1);
-	t3.start();
+//	MessageSender ms1=new MessageSender("10.8.102.34");
+//
+//
+//	Thread t3=new Thread(ms1);
+//	t3.start();
 	}
 
 }
