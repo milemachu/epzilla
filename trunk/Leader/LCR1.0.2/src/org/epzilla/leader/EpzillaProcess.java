bac575d8bfed8814116130bbc79ac7a088a04f76
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-02-28 08:32:13 +0000

    This is the latest version with some more things are completed.

---
+++ trunk/Leader/LCR1.0.2/src/org/epzilla/leader/EpzillaProcess.java
@@ -0,0 +1,91 @@
+package org.epzilla.leader;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Vector;
+
+public class EpzillaProcess {
+
+	// This is the UID of this host
+	public static byte UID = 1;
+
+	private String status;
+	private boolean isDefaultLeader;
+	private InetAddress clusterLeader;
+	private Vector<InetAddress> clusterIpList;
+	private Vector<InetAddress> clusterAliveIpList;
+
+	// Private constructor prevents instantiation from other classes
+	private EpzillaProcess() {
+		try {
+			//TODO change these according to the node
+			setStatus(Status.UNKNOWN.toString());
+			setDefaultLeader(true);
+			setClusterLeader(InetAddress.getLocalHost());
+			//Add these from some where 
+			setClusterIpList(new Vector<InetAddress>());
+//			getClusterIpList().add(e)
+			setClusterAliveIpList(new Vector<InetAddress>());
+			//Get these from the STM and update later. intiall none
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * SingletonHolder is loaded on the first execution of
+	 * Singleton.getInstance() or the first access to SingletonHolder.INSTANCE,
+	 * not before.
+	 */
+	private static class EpzillaProcessHolder {
+		private static final EpzillaProcess INSTANCE = new EpzillaProcess();
+	}
+
+	public static EpzillaProcess getInstance() {
+		return EpzillaProcessHolder.INSTANCE;
+	}
+
+	public synchronized String getStatus() {
+		return status;
+	}
+
+	public synchronized void setStatus(String status) {
+
+		this.status = status;
+	}
+
+	public synchronized void setDefaultLeader(boolean isDefaultLeader) {
+		this.isDefaultLeader = isDefaultLeader;
+	}
+
+	public synchronized boolean isDefaultLeader() {
+		return isDefaultLeader;
+	}
+
+	public void setClusterLeader(InetAddress clusterLeader) {
+		this.clusterLeader = clusterLeader;
+	}
+
+	public InetAddress getClusterLeader() {
+		return clusterLeader;
+	}
+
+	public void setClusterIpList(Vector<InetAddress> clusterIpList) {
+		this.clusterIpList = clusterIpList;
+	}
+
+	public Vector<InetAddress> getClusterIpList() {
+		return clusterIpList;
+	}
+
+	public void setClusterAliveIpList(Vector<InetAddress> clusterAliveIpList) {
+		this.clusterAliveIpList = clusterAliveIpList;
+	}
+
+	public Vector<InetAddress> getClusterAliveIpList() {
+		return clusterAliveIpList;
+	}
+
+}
