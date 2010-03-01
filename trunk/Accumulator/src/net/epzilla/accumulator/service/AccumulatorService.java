47574680f8d5926fddf5d624f004437132e2aaa7
rjvranga <rjvranga@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-01 08:27:55 +0000

    just an outline. no impl.

---
+++ trunk/Accumulator/src/net/epzilla/accumulator/service/AccumulatorService.java
@@ -0,0 +1,21 @@
+package net.epzilla.accumulator.service;
+
+import net.epzilla.accumulator.global.DerivedEvent;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajeev
+ * Date: Feb 28, 2010
+ * Time: 7:52:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AccumulatorService extends Remote {
+
+        public void receiveDerivedEvent(DerivedEvent event) throws RemoteException;
+
+    
+
+}
