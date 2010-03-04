e9c883ef4ff4052c3769fa0fb77d7702efc60b08
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-04 16:31:01 +0000

    updated

--- trunk/src/org/epzilla/ui/ClientUIControler.java
+++ trunk/src/org/epzilla/ui/ClientUIControler.java
@@ -2,18 +2,23 @@ package org.epzilla.ui;
 
 import javax.swing.JFrame;
 
-import org.epzilla.ui.*;
-
-public class ClientUIControler {
-	private static ClientUI clientInstance;
-	
-	  public static void InitializeUI() {
-	        clientInstance = new ClientUI();
-	        clientInstance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        clientInstance.setVisible(true);
-	    }
-	  public static void appendTextToStatus(String text) {
-	        clientInstance.getTxtResults().append(text + "\n");
-	    }
+public class ClientUIControler implements Runnable{
+		private static ClientUI clientInstance;
+		String message = "";
+		
+		public ClientUIControler(){
+		}
+		public ClientUIControler(String msg){
+			this.message = msg;
+		}
+		public static void initializeClientUI(){
+			clientInstance = new ClientUI();
+			clientInstance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			clientInstance.setVisible(true);
+		}
+	  @Override
+	  public void run() {
+		  clientInstance.getTxtResults().append(message + "\n");
+	  }
 
 }
