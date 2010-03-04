207690bbb312f350417ff730cc1a609c5bdcbb99
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-04 16:36:01 +0000

    updated

--- trunk/src/org/epzilla/dispatcher/DispInterface.java
+++ trunk/src/org/epzilla/dispatcher/DispInterface.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import org.epzilla.ui.ClientCallbackInterface;
+
 public interface DispInterface extends Remote {
 
 	public String  uploadEventsToDispatcher(byte[] stream,String cID,int eventSeqID) throws RemoteException;
 	public String uploadTriggersToDispatcher(byte[] stream,String cID,int triggerSeqID) throws RemoteException;
 	public byte[] downloadFileFromServer(String fileName) throws RemoteException, IOException;
-	public void acceptNotifications()throws RemoteException;
+	public String acceptNotifications()throws RemoteException;
+	public void registerCallback(ClientCallbackInterface callbackClientObject) throws RemoteException;
+	public void unregisterCallback(ClientCallbackInterface callbackClientObject)throws RemoteException;
+
 }
