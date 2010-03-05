8d0e5ac20fdb3ce590084f9dd73e78517a2b89a4
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-05 21:55:53 +0000

    Handled the PULSE and PING issues and started the now authorized. ToDo:complete the not authorized with Reject Reason.

---
+++ Leader/LCR1.0.9/src/org/epzilla/leader/event/PulseReceivedEvent.java
@@ -0,0 +1,5 @@
+package org.epzilla.leader.event;
+
+public class PulseReceivedEvent implements IEpzillaEvent {
+
+}
---
+++ Leader/LCR1.0.9/src/org/epzilla/leader/rmi/LeaderInterface.java
@@ -0,0 +1,88 @@
+package org.epzilla.leader.rmi;
+
+import java.net.UnknownHostException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import org.epzilla.leader.event.IEpzillaEventListner;
+
+public interface LeaderInterface extends Remote {
+
+	/**
+	 * 
+	 * @return the UID assigned to this machine by Administrator
+	 * @throws RemoteException
+	 */
+	public int getUID() throws RemoteException;
+
+	/**
+	 * When the leader election is completed and a leader is elected this method
+	 * will be called by the member in the right hand side (above the list)
+	 * 
+	 * @param Leader
+	 *            Election Message
+	 * @throws RemoteException
+	 * @throws UnknownHostException
+	 */
+	public void electedLeader(String message) throws RemoteException,
+			UnknownHostException;
+
+	/**
+	 * If some other node need to find out whether this node is the leader, this
+	 * is called.
+	 * 
+	 * @return boolean whether the leader or not
+	 * @throws RemoteException
+	 */
+	public boolean isLeader() throws RemoteException;
+
+	/**
+	 * This is to be called to get the status of the node
+	 * 
+	 * @return Status of the node
+	 * @throws RemoteException
+	 */
+	public String getStatus() throws RemoteException;
+
+	/**
+	 * This is general method to pass the messages from host to host. This
+	 * message will be decoded and will be passed to the Busi. Logic.
+	 * 
+	 * @param message
+	 * @throws RemoteException
+	 * @throws UnknownHostException
+	 */
+	public void receiveMessage(String message) throws RemoteException,
+			UnknownHostException;
+
+	/**
+	 * There is a default leader declared for every STM based clusters. This
+	 * method will tell whether this node is the default leader or not. Any node
+	 * can invoke this method.
+	 * 
+	 * @throws RemoteException
+	 */
+	public boolean isDefaultLeader() throws RemoteException;
+	
+	/**
+	 * This method is used to register the epzilla event listeners in leader for pulse,etc
+	 * @param listener
+	 * @throws RemoteException
+	 */
+	public void addListener(IEpzillaEventListner listener) throws RemoteException;
+	
+	/**
+	 * This method is used to unregister a given epzilla event listeners from the leader.
+	 * @param listener
+	 * @throws RemoteException
+	 */
+	public void removeListener(IEpzillaEventListner listener) throws RemoteException;
+	
+	/**
+	 * This is used to get the Leader address from a remote machine without running a Leader Election.
+	 * @return
+	 * @throws RemoteException
+	 */
+	public String getClusterLeader() throws RemoteException;
+
+}
