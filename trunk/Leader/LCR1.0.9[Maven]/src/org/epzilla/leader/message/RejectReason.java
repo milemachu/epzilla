43ee309eef9de81fb479c1c84ea93ec496ba40c3
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-06 13:53:26 +0000

    Maven integrated for automated building and automated testing. Possible to open with Idea as well.

---
+++ trunk/Leader/LCR1.0.9[Maven]/src/org/epzilla/leader/message/RejectReason.java
@@ -0,0 +1,12 @@
+package org.epzilla.leader.message;
+
+/**
+ * This class is used to define the reasons for the message 6-NOT ACCEPTED HERE.
+ * Reply Code from this class will be attached with the Type 6 reply to get the requester to
+ * identify the rejected reason. 
+ * @author Administrator
+ *
+ */
+public class RejectReason {
+
+}
