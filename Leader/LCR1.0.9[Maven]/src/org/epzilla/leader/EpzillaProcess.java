6c28160a274d8a1ffac64630210498a510c1f6e6
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-06 13:53:26 +0000

    Maven integrated for automated building and automated testing. Possible to open with Idea as well.

---
+++ Leader/LCR1.0.9[Maven]/src/org/epzilla/leader/EpzillaProcess.java
@@ -0,0 +1,235 @@
+package org.epzilla.leader;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.Vector;
+
+import org.epzilla.leader.event.EpzillaPulseReceiveTimer;
+import org.epzilla.leader.event.EpzillaPulseSendTimer;
+import org.epzilla.leader.event.IEpzillaEvent;
+import org.epzilla.leader.event.IEpzillaEventListner;
+import org.epzilla.leader.event.PingReceivedEvent;
+import org.epzilla.leader.event.ProcessStatusChangedEvent;
+import org.epzilla.leader.event.PulseIntervalTimeoutEvent;
+import org.epzilla.leader.event.PulseNotReceivedTimeoutEvent;
+import org.epzilla.leader.event.PulseReceivedEvent;
+import org.epzilla.leader.rmi.LeaderMessageClient;
+
+public class EpzillaProcess {
+
+	// This is the UID of this host
+	public static byte UID = 1;
+
+	private String status;
+	private boolean isDefaultLeader;
+	private InetAddress clusterLeader;
+	private Vector<InetAddress> clusterIpList;
+	private Vector<InetAddress> clusterAliveIpList;
+	private final Timer timer=new Timer();
+
+	// Private constructor prevents instantiation from other classes
+	private EpzillaProcess() {
+		try {
+			// TODO change these according to the node
+			setStatus(Status.UNKNOWN.toString());
+			setDefaultLeader(true);
+			setClusterLeader(InetAddress.getLocalHost());
+			// Add these from some where
+			setClusterIpList(new Vector<InetAddress>());
+			// getClusterIpList().add(e)
+			// Get these from the STM and update later. initially none
+			setClusterAliveIpList(new Vector<InetAddress>());
+			
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * SingletonHolder is loaded on the first execution of
+	 * Singleton.getInstance() or the first access to SingletonHolder.INSTANCE,
+	 * not before.
+	 */
+	private static class EpzillaProcessHolder {
+		private static final EpzillaProcess INSTANCE = new EpzillaProcess();
+	}
+
+	public static EpzillaProcess getInstance() {
+		return EpzillaProcessHolder.INSTANCE;
+	}
+
+	public synchronized String getStatus() {
+		return status;
+	}
+
+	public synchronized void setStatus(String status) {
+
+		this.status = status;
+	}
+
+	public synchronized void setDefaultLeader(boolean isDefaultLeader) {
+		this.isDefaultLeader = isDefaultLeader;
+	}
+
+	public synchronized boolean isDefaultLeader() {
+		return isDefaultLeader;
+	}
+
+	public synchronized void setClusterLeader(InetAddress clusterLeader) {
+		this.clusterLeader = clusterLeader;
+	}
+
+	public synchronized InetAddress getClusterLeader() {
+		return clusterLeader;
+	}
+
+	public synchronized void setClusterIpList(Vector<InetAddress> clusterIpList) {
+		this.clusterIpList = clusterIpList;
+	}
+
+	public synchronized Vector<InetAddress> getClusterIpList() {
+		return clusterIpList;
+	}
+
+	public synchronized void setClusterAliveIpList(
+			Vector<InetAddress> clusterAliveIpList) {
+		this.clusterAliveIpList = clusterAliveIpList;
+	}
+
+	public synchronized Vector<InetAddress> getClusterAliveIpList() {
+		return clusterAliveIpList;
+	}
+
+	public synchronized void addEpzillaEventListener(
+			IEpzillaEventListner listener) {
+		if (EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(
+				Status.LEADER.toString()))
+			EpzillaLeader.getInstance().getClusterClientList().add(listener);
+	}
+
+	public synchronized void removeEpzillaEventListener(
+			IEpzillaEventListner listener) {
+		if (EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(
+				Status.LEADER.toString())
+				&& EpzillaLeader.getInstance().getClusterClientList() != null) {
+			EpzillaLeader.getInstance().getClusterClientList().removeElement(
+					listener);
+		}
+	}
+
+	public synchronized void fireEpzillaEvent(IEpzillaEvent event) {		
+
+		if (event instanceof ProcessStatusChangedEvent) {
+			// Check whether the new status is nonLeader or Unknown
+			// If above true, reset timer and epZilla leader
+			if (!EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(
+					Status.LEADER.toString())) {
+				EpzillaLeader.getInstance().setClusterClientList(null);
+				timer.cancel();
+			}
+			System.out.println("Epzilla Process Status changed Event Fired.");
+		} else if (event instanceof PulseIntervalTimeoutEvent) {
+			// Send a pulse to each and every listener
+			if (EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(
+					Status.LEADER.toString())) {
+				Iterator<IEpzillaEventListner> iterator = EpzillaLeader
+						.getInstance().getClusterClientList().listIterator();
+				while (iterator.hasNext()) {
+					IEpzillaEventListner listner = (IEpzillaEventListner) iterator
+							.next();
+
+					// PULSE message sent from here
+					try {
+						LeaderMessageClient.sendPulse(listner.getData());
+					} catch (MalformedURLException e) {
+
+						e.printStackTrace();
+					} catch (RemoteException e) {
+
+						e.printStackTrace();
+					} catch (UnknownHostException e) {
+
+						e.printStackTrace();
+					} catch (NotBoundException e) {
+
+						e.printStackTrace();
+					}
+				}
+
+				// TODO Implement and RESET the Timer for Pulse
+				timer.schedule(new EpzillaPulseSendTimer(), 240000);
+				System.out.println("Pulse sending time out event fired.");
+			}
+		} else if (event instanceof PulseNotReceivedTimeoutEvent) {
+			// Client has not received the pulse after 360 default interval.
+			//So send a PING to the last known Leader ad check.
+			//If the Leader is the same node, it will send a pulse. Else have to get the new leader
+			//From another host. No need for a Leader election.
+			//TODO Think abt this more before complete.
+				try {
+					if(EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(Status.NON_LEADER.toString())){
+					LeaderMessageClient.sendPing(EpzillaProcess.getInstance().getClusterLeader().getHostAddress());
+					}else{
+//						EpzillaProcess.getInstance().setClusterLeader(LeaderMessageClient.getClusterLeaderFromRemote(remoteIp))
+					}
+				} catch (MalformedURLException e) {
+					
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					
+					e.printStackTrace();
+				}
+			
+		} else if (event instanceof PingReceivedEvent) {
+			// If leader, send a pulse, else send request not accepted.
+
+			try {
+				if (EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(
+						Status.LEADER.toString())) {
+					LeaderMessageClient.sendPulse(((PingReceivedEvent) event)
+							.getData());
+				} else {
+					LeaderMessageClient
+							.sendRequestNotAccepted(((PingReceivedEvent) event)
+									.getData());
+				}
+			} catch (MalformedURLException e) {
+				
+				e.printStackTrace();
+			} catch (RemoteException e) {
+				
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+			
+				e.printStackTrace();
+			} catch (NotBoundException e) {
+			
+				e.printStackTrace();
+			}
+
+		}else if(event instanceof PulseReceivedEvent){
+			//Check the validity of receiving of this.
+			if(EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(Status.NON_LEADER.toString())){
+				//Authorized and valid. So reset the timer for pulse receive.
+				timer.cancel();
+				timer.schedule(new EpzillaPulseReceiveTimer(), 360000);
+				
+			}else{
+				//TODO Invalid or unAutorized. Reject it.send unauth
+			}
+		}
+	}
+
+}
---
+++ Leader/LCR1.0.9[Maven]/src/org/epzilla/leader/message/Message.java
@@ -0,0 +1,187 @@
+package org.epzilla.leader.message;
+
+import java.net.InetAddress;
+
+import org.epzilla.leader.EpzillaProcess;
+
+/**
+ * This class defines the message format to be sent When the parameters are
+ * given, it will return the message
+ * 
+ * @author Administrator
+ * 
+ */
+public class Message {
+
+	private Message() {
+	}
+
+	/**
+	 * SingletonHolder is loaded on the first execution of
+	 * Singleton.getInstance() or the first access to SingletonHolder.INSTANCE,
+	 * not before.
+	 */
+	private static class MessageHolder {
+		private static final Message INSTANCE = new Message();
+	}
+
+	public static Message getInstance() {
+		return MessageHolder.INSTANCE;
+	}
+
+	/**
+	 * MESSAGEId Separator || Separator IP Separator Status Separator
+	 */
+
+	public String getUidMessage() {
+		/**
+		 * MESSAGEId Separator |UID| Separator IP Separator Status Separator
+		 */
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			sb.append(MessageMeta.UID).append(MessageMeta.SEPARATOR).append(
+					EpzillaProcess.UID).append(MessageMeta.SEPARATOR);
+			sb.append(InetAddress.getLocalHost().getHostAddress()).append(
+					MessageMeta.SEPARATOR).append(
+					EpzillaProcess.getInstance().getStatus());
+			sb.append(MessageMeta.SEPARATOR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+	public String getLeaderPublishMessage() {
+		/**
+		 * MESSAGEId Separator IP Separator
+		 */
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			sb.append(MessageMeta.LEADER).append(MessageMeta.SEPARATOR).append(
+					InetAddress.getLocalHost().getHostAddress()).append(
+					MessageMeta.SEPARATOR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+	public String getNonLeaderMessage() {
+		/**
+		 * MESSAGEId Separator IP Separator
+		 */
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			sb.append(MessageMeta.NON_LEADER).append(MessageMeta.SEPARATOR)
+					.append(InetAddress.getLocalHost().getHostAddress())
+					.append(MessageMeta.SEPARATOR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+	public String getPulse() {
+
+		/**
+		 * MESSAGEId Separator IP Separator
+		 */
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			sb.append(MessageMeta.PULSE).append(MessageMeta.SEPARATOR).append(
+					InetAddress.getLocalHost().getHostAddress()).append(
+					MessageMeta.SEPARATOR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+
+	}
+
+	public String getPing() {
+		/**
+		 * MESSAGEId Separator IP Separator
+		 */
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			sb.append(MessageMeta.PING_LEADER).append(MessageMeta.SEPARATOR)
+					.append(InetAddress.getLocalHost().getHostAddress())
+					.append(MessageMeta.SEPARATOR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+
+	}
+	
+	public String getRequestNotAccepted(){
+		/**
+		 * MESSAGEId Separator IP Separator
+		 */
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			sb.append(MessageMeta.REQUEST_NOT_ACCEPTED).append(MessageMeta.SEPARATOR)
+					.append(InetAddress.getLocalHost().getHostAddress())
+					.append(MessageMeta.SEPARATOR);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+
+	}
+
+	/**
+	 * Return the message type according to the header value. When a new message
+	 * type added, place the message Id here
+	 * 
+	 * @param messageCode
+	 * @return if a valid message => message type, if not => null
+	 */
+	public String getMessage(int messageCode) {
+		String message = null;
+
+		switch (messageCode) {
+		case 1:
+			message = "UID";
+			break;
+
+		case 2:
+			message = "LEADER";
+			break;
+
+		case 3:
+			message = "NON_LEADER";
+			break;
+
+		case 4:
+			message = "PULSE";
+			break;
+
+		case 5:
+			message = "PING";
+			break;
+			
+		case 6:
+			message="REQUEST_NOT_ACCEPTED";
+			break;
+
+		default:
+			break;
+		}
+
+		return message;
+	}
+
+}
