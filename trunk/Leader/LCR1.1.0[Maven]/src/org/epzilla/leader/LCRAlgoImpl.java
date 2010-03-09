a0e4c1146eec054d5aa113bbecf854ac7a3399a1
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-09 21:06:37 +0000

    LCR Leader Election Algorithm complete implementation. TODO: Create test cases and Javadocs. Test in the real environment and Optimizations.

---
+++ trunk/Leader/LCR1.1.0[Maven]/src/org/epzilla/leader/LCRAlgoImpl.java
@@ -0,0 +1,133 @@
+package org.epzilla.leader;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Vector;
+
+import org.epzilla.leader.event.IEpzillaEventListner;
+import org.epzilla.leader.event.ProcessStatusChangedEvent;
+import org.epzilla.leader.message.MessageMeta;
+import org.epzilla.leader.rmi.LeaderMessageClient;
+import org.epzilla.leader.util.Status;
+
+/**
+ * This class contains the LCR leader election algorithm implementation. All the
+ * necessary LCR specific methods are here.
+ * Based on the assumption that 1 is the highest priority in UID sequence.
+ * 
+ * @author Administrator
+ * 
+ */
+public class LCRAlgoImpl {
+
+	// Private constructor prevents instantiation from other classes
+	private LCRAlgoImpl() {
+	}
+
+	/**
+	 * SingletonHolder is loaded on the first execution of
+	 * Singleton.getInstance() or the first access to SingletonHolder.INSTANCE,
+	 * not before.
+	 */
+	private static class LCRAlgoImplHolder {
+		private static final LCRAlgoImpl INSTANCE = new LCRAlgoImpl();
+	}
+
+	public static LCRAlgoImpl getInstance() {
+		return LCRAlgoImplHolder.INSTANCE;
+	}
+
+	// This is the LCR logic
+	public void runAlgorithm(String message) {
+		String[] strArray = message.split(Character
+				.toString(MessageMeta.SEPARATOR));
+
+		if (strArray != null && Integer.parseInt(strArray[1]) >= 1) {
+			int receivedUid = Integer.parseInt(strArray[1]);
+			if (receivedUid == EpzillaProcess.UID) {
+				// This is the Leader. Send the Leader message to others
+				// UPDATE the local variables such as ClusterLeader and Status
+				try {
+
+					EpzillaProcess.getInstance().setClusterLeader(
+							InetAddress.getByName(strArray[2]));
+					EpzillaProcess.getInstance().setStatus(
+							Status.LEADER.toString());
+					EpzillaProcess.getInstance().fireEpzillaEvent(
+							new ProcessStatusChangedEvent());
+					EpzillaProcess.getInstance().setLeaderElectionRunning(false);					
+
+					// Init epZilla Leader and ready to receive listeners
+					EpzillaLeader.getInstance().setClusterClientList(
+							new Vector<IEpzillaEventListner>());
+					System.out
+							.println("Cluster leader set as the localhost and Status changed event fired.");
+
+					// remove the hardcode address and get it from the ip
+					// list
+					LeaderMessageClient
+							.sendLeaderElectedMessage(EpzillaProcess.getInstance().getIpNextToThis());
+					System.out.println("Leader published as this host.");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (receivedUid < EpzillaProcess.UID) {
+				// Pass the message to next node
+				// If we want we can declare this as a non leader since this is
+				// going to be declared as a non leader upon the receiving the
+				// Leader published message.
+
+				try {
+					//  remove hardcode and get from the ip list
+					LeaderMessageClient.forwardReceivedUidMessage(EpzillaProcess.getInstance().getIpNextToThis(), message);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (receivedUid > EpzillaProcess.UID) {
+				// No Forwarding of received message.Instead start sending the
+				// UID of this
+				//  Remove the hardcode and get it from the list
+				try {
+					LeaderMessageClient.sendUidMessage(EpzillaProcess.getInstance().getIpNextToThis());
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+}
---
+++ trunk/Leader/LCR1.1.0[Maven]/src/org/epzilla/leader/rmi/LeaderMessageClient.java
@@ -0,0 +1,325 @@
+package org.epzilla.leader.rmi;
+
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.epzilla.leader.event.IEpzillaEventListner;
+import org.epzilla.leader.message.Message;
+import org.epzilla.leader.message.MessageMeta;
+
+public class LeaderMessageClient {
+
+	/**
+	 * @param args
+	 * @throws NotBoundException
+	 * @throws RemoteException
+	 * @throws MalformedURLException
+	 * @throws UnknownHostException
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws MalformedURLException,
+			RemoteException, NotBoundException, UnknownHostException,
+			InterruptedException {
+//		LeaderInterface li = (LeaderInterface) Naming
+//				.lookup("rmi://"+InetAddress.getLocalHost().getHostAddress()+"/LeaderService");
+		
+//		LeaderInterface li=null;
+//		
+//
+//		try {
+//			System.out.println(System.currentTimeMillis());
+//			li = (LeaderInterface) Naming
+//			.lookup("rmi://"+"192.168.1.1"+"/LeaderService");
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			System.out.println(System.currentTimeMillis());
+//			e.printStackTrace();
+//		}
+//		System.out.println(li.isLeaderElectionRunning());
+
+//		System.out.println("Via RMI : is Leader: " + li.isLeader());
+//		System.out.println("Via RMI : is Default Leader: "
+//				+ li.isDefaultLeader());
+//		
+//		
+//
+//		li.receiveMessage(Message.getInstance().getUidMessage());
+//
+//		Thread.currentThread();
+//		Thread.sleep(5000);
+//
+//		li.electedLeader(Message.getInstance().getLeaderPublishMessage());
+//
+//		System.out.println("Via RMI: is Leader: " + li.isLeader());
+//		System.out.println("Via RMI: get Status: " + li.getStatus());
+//		System.out.println("Via Static: get Cluster leader: "
+//				+ EpzillaProcess.getInstance().getClusterLeader());
+//		System.out.println("Via Static: get Status: "
+//				+ EpzillaProcess.getInstance().getStatus());
+//		
+//		li.receiveMessage(Message.getInstance().getRequestNotAccepted()+402+MessageMeta.SEPARATOR);
+//		li.receiveMessage(Message.getInstance().getRequestNotAccepted()+401+MessageMeta.SEPARATOR);
+//		li.receiveMessage(Message.getInstance().getRequestNotAccepted()+400+MessageMeta.SEPARATOR);
+
+	}
+
+	/**
+	 * This method will return the search for the Leader interface and and
+	 * return the Leader interface reference of a remote node
+	 * 
+	 * @param remoteIp
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 */
+	private static LeaderInterface getLeaderInterface(String remoteIp)
+			throws MalformedURLException, RemoteException, NotBoundException {
+
+		LeaderInterface li = (LeaderInterface) Naming.lookup("rmi://"
+				+ remoteIp + "/LeaderService");
+
+		return li;
+	}
+
+	/**
+	 * This is the method used to send the Leader publish message
+	 * 
+	 * @param remoteIp
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static void sendLeaderElectedMessage(String remoteIp)
+			throws MalformedURLException, RemoteException, NotBoundException,
+			UnknownHostException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.electedLeader(Message.getInstance().getLeaderPublishMessage());
+		// Local host declared as the Leader and message is sent
+		// TODO remove
+		System.out.println(li.getStatus());
+
+		// TODO Remove
+		System.out
+				.println("Leader message published for this node since this is the Leader and sent to:"
+						+ remoteIp);
+
+	}
+
+	/**
+	 * This method is used to forward the leader published message
+	 * 
+	 * @param remoteIp
+	 * @param message
+	 * @throws NotBoundException
+	 * @throws RemoteException
+	 * @throws MalformedURLException
+	 * @throws UnknownHostException
+	 */
+	public static void forwardLeaderElectedMessage(String remoteIp,
+			String message) throws MalformedURLException, RemoteException,
+			NotBoundException, UnknownHostException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.receiveMessage(message);
+
+		// TODO Remove
+		System.out
+				.println("Leader elected message forwarded since this node is in Non_Leader. Forwarded to:"
+						+ remoteIp);
+	}
+
+	/**
+	 * This is the method used to send the LCR algo message when the message uid
+	 * is higher than self
+	 * 
+	 * @param remoteIp
+	 * @param receivedMessage
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static void forwardReceivedUidMessage(String remoteIp,
+			String receivedMessage) throws MalformedURLException,
+			RemoteException, NotBoundException, UnknownHostException {
+
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.receiveMessage(receivedMessage);
+
+		System.out.println("The UID of lower order forwarded to:" + remoteIp);
+
+	}
+
+	/**
+	 * This method is used to send the UID of this node if the received UID <
+	 * self UID
+	 * 
+	 * @param remoteIp
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static void sendUidMessage(String remoteIp)
+			throws MalformedURLException, RemoteException, NotBoundException,
+			UnknownHostException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.receiveMessage(Message.getInstance().getUidMessage());
+
+		System.out.println("Recived UID is higher or same order. This UID sent to:"
+				+ remoteIp);
+	}
+
+	/**
+	 * This method is used to send a pulse to non leader nodes in a timely
+	 * manner. Will be used only by Leaders.
+	 * 
+	 * @param remoteIp
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static void sendPulse(String remoteIp) throws MalformedURLException,
+			RemoteException, NotBoundException, UnknownHostException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.receiveMessage(Message.getInstance().getPulse());
+
+		System.out.println("Pulse sent to the non leader client:" + remoteIp);
+	}
+
+	/**
+	 * This method is used to register the non leader epzilla event listeners
+	 * with the Leader. Will only be used by Non Leader nodes.
+	 * 
+	 * @param remoteIp
+	 * @param listener
+	 * @throws RemoteException
+	 * @throws MalformedURLException
+	 * @throws NotBoundException
+	 */
+	public static void registerListenerWithLeader(String remoteIp,
+			IEpzillaEventListner listener) throws RemoteException,
+			MalformedURLException, NotBoundException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.addListener(listener);
+
+		System.out
+				.println("Listner registered with the Leader and ready to listen to pulse.");
+	}
+
+	/**
+	 * This method is used to unregister the non leader epzilla event listener
+	 * with the leader. Will only be used by the Non leader nodes.
+	 * 
+	 * @param remoteIp
+	 * @param listener
+	 * @throws RemoteException
+	 * @throws MalformedURLException
+	 * @throws NotBoundException
+	 */
+	public static void unregisterListenerWithLeader(String remoteIp,
+			IEpzillaEventListner listener) throws RemoteException,
+			MalformedURLException, NotBoundException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.removeListener(listener);
+
+		System.out.println("Listener unregistered from the leader.");
+
+		// TODO Might not work since we have not overridden the hashcode() and
+		// equals() of IEpzillaEventListner implementation class.
+	}
+
+	/**
+	 * If s host is unable to accepted a message to not authorized to reply, this message is sent. 
+	 * @param remoteIp
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static void sendRequestNotAccepted(String remoteIp, int errorCode) throws MalformedURLException, RemoteException, NotBoundException, UnknownHostException {
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.receiveMessage(Message.getInstance().getRequestNotAccepted()+errorCode+MessageMeta.SEPARATOR);
+
+		System.out.println("Request not accepted sent to:" + remoteIp);
+	}
+	
+	/**
+	 * This is used to send Ping message to a given node.
+	 * @param remoteIp
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static void sendPing(String remoteIp) throws MalformedURLException, RemoteException, NotBoundException, UnknownHostException{
+		LeaderInterface li = getLeaderInterface(remoteIp);
+
+		li.receiveMessage(Message.getInstance().getPing());
+
+		System.out.println("Ping sent to:" + remoteIp);
+	}
+	
+	/**
+	 * This is used to get the Leader address from a remote machine without running a Leader Election.
+	 * @param remoteIp
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 * @throws UnknownHostException
+	 */
+	public static String getClusterLeaderFromRemote(String remoteIp) throws MalformedURLException, RemoteException, NotBoundException, UnknownHostException{
+		LeaderInterface li = getLeaderInterface(remoteIp);		
+
+		System.out.println("get cluster leader sent to:" + remoteIp);
+		
+		return li.getClusterLeader();
+	}
+	
+	/**
+	 * This method is used to get the status of a remote machine.
+	 * @param remoteIp
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 */
+	public static String getStateFromRemote(String remoteIp) throws MalformedURLException, RemoteException, NotBoundException{
+		LeaderInterface li = getLeaderInterface(remoteIp);		
+
+		System.out.println("get cluster leader sent to:" + remoteIp);
+		
+		return li.getStatus();
+	}
+	
+	/**
+	 * This is used to check whether the LE is running in a remote host. 
+	 * @param remoteIp
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws RemoteException
+	 * @throws NotBoundException
+	 */
+	public static boolean isLeaderElectioRunningInRemote(String remoteIp) throws MalformedURLException, RemoteException, NotBoundException{
+		LeaderInterface li=getLeaderInterface(remoteIp);
+		
+		System.out.println("get is Leader running in remote sent to:"+remoteIp);
+		
+		return li.isLeaderElectionRunning();
+	}
+
+}
