dce831a86cf95da8a4e72bd2fc8460998080944c
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-09 21:06:37 +0000

    LCR Leader Election Algorithm complete implementation. TODO: Create test cases and Javadocs. Test in the real environment and Optimizations.

---
+++ Leader/LCR1.1.0[Maven]/src/org/epzilla/leader/EpzillaAltNodeInitiator.java
@@ -0,0 +1,72 @@
+package org.epzilla.leader;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.log4j.Logger;
+import org.epzilla.leader.rmi.LeaderRMIServer;
+import org.epzilla.leader.util.ConfigurationLoader;
+
+public class EpzillaAltNodeInitiator {
+
+	private static Logger logger = Logger.getLogger(EpzillaAltNodeInitiator.class);
+	private static EpzillaProcess epzillaInstance;
+	
+public EpzillaAltNodeInitiator() {
+		epzillaInstance=EpzillaProcess.getInstance();
+		
+	}
+	
+	/**
+	 * Use this method to deploy all the RMI services.
+	 * @return
+	 */
+	private boolean deployRmiInterace(){
+		try {
+			logger.info("Deploying RMI Services in:"+InetAddress.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return LeaderRMIServer.deployLeaderService();
+	}
+	
+	public static void main(String[] args) {
+		EpzillaAltNodeInitiator initiator=new EpzillaAltNodeInitiator();
+		
+		
+		//LOAD CONFIGS via Configuration loader.
+		try {
+			new ConfigurationLoader().loadConfig(EpzillaProcess.getInstance());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		boolean isServicesDeployed=initiator.deployRmiInterace();
+		
+		if(isServicesDeployed){
+			logger.info("RMI Services deployied. Initializing start up sequence.");
+
+		boolean isDefaultLeader=epzillaInstance.isDefaultLeader();
+		
+		if(isDefaultLeader){
+			//This might be a start up of the DL or returning of the DL
+			//Have to poll other machines and fins out whether they are running a LE or have a Leader or they also 
+			//just started. Use isLeaderRunning and Status to identify the situation.
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}else{
+		logger.info("RMI service deployment failed. System shutting down.");
+		System.exit(-1);
+	}
+}
+}
---
+++ Leader/LCR1.1.0[Maven]/src/org/epzilla/leader/message/MessageDecoder.java
@@ -0,0 +1,158 @@
+package org.epzilla.leader.message;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.epzilla.leader.EpzillaProcess;
+import org.epzilla.leader.LCRAlgoImpl;
+import org.epzilla.leader.event.EpZillaListener;
+import org.epzilla.leader.event.PingReceivedEvent;
+import org.epzilla.leader.event.ProcessStatusChangedEvent;
+import org.epzilla.leader.event.PulseIntervalTimeoutEvent;
+import org.epzilla.leader.event.PulseReceivedEvent;
+import org.epzilla.leader.event.RequestRejectedEvent;
+import org.epzilla.leader.rmi.LeaderMessageClient;
+import org.epzilla.leader.util.Status;
+
+/**
+ * This class will decode the incoming message from the others to RMI server of
+ * local host. It will send the result to the Business Logic of the algorithm
+ * and do the work.
+ * 
+ * @author Administrator
+ * 
+ */
+public class MessageDecoder {
+
+	// Private constructor prevents instantiation from other classes
+	private MessageDecoder() {
+	}
+
+	/**
+	 * SingletonHolder is loaded on the first execution of
+	 * Singleton.getInstance() or the first access to SingletonHolder.INSTANCE,
+	 * not before.
+	 */
+	private static class MessageDecorderHolder {
+		private static final MessageDecoder INSTANCE = new MessageDecoder();
+	}
+
+	public static MessageDecoder getInstance() {
+		return MessageDecorderHolder.INSTANCE;
+	}
+
+	// This method will decode the message
+	public void decodeMessage(String message) throws UnknownHostException {
+		String[] strItems = message.split(Character
+				.toString(MessageMeta.SEPARATOR));
+
+		String messageType = Message.getInstance().getMessage(
+				Integer.parseInt(strItems[0]));
+		System.out.println("New Message Received: " + messageType);
+
+		// Logic here to pass the message type with parameters to B.L.
+
+		// Elected leader send message
+		if (Integer.parseInt(strItems[0]) == MessageMeta.LEADER) {
+			// This message is from Leader and to Leader publish msg and
+			// received at electedLeader.
+
+			if (strItems[1].equalsIgnoreCase(InetAddress.getLocalHost()
+					.getHostAddress())) {
+
+				System.out.println("Leader is the Localhost");
+				// No need to forward the LEADER message. Do nothing now about
+				// that message.
+				// Set the timer for Pulse
+				EpzillaProcess.getInstance().fireEpzillaEvent(
+						new PulseIntervalTimeoutEvent());
+			} else {
+				EpzillaProcess.getInstance().setClusterLeader(
+						InetAddress.getByName(strItems[1]));
+				EpzillaProcess.getInstance().setStatus(
+						Status.NON_LEADER.toString());
+				EpzillaProcess.getInstance().fireEpzillaEvent(
+						new ProcessStatusChangedEvent());
+				EpzillaProcess.getInstance().setLeaderElectionRunning(false);
+				System.out
+						.println("Leader is else and this is  a NON leader. Status changed event fired.");
+
+				// Forward the Leader message to next host and register this
+				// node as a listener.
+				try {
+
+					// TODO Remove hard code and get it from the list
+					LeaderMessageClient.forwardLeaderElectedMessage(EpzillaProcess.getInstance().getIpNextToThis(), message);
+					LeaderMessageClient.registerListenerWithLeader(strItems[1],
+							new EpZillaListener());
+					
+					//Set the pulse receive timer here for the first time
+					//cannot remove since we register the listener and server supposed to send a pulse for real.
+					//But it is possible that 1st pulse also not receiving.
+					EpzillaProcess.getInstance().fireEpzillaEvent(new PulseReceivedEvent(strItems[1]));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+			System.out
+					.println("Leader published and updated at the local instance.");
+			System.out.println("Leader is "
+					+ EpzillaProcess.getInstance().getClusterLeader());
+			System.out.println("Local host is " + InetAddress.getLocalHost());
+			System.out.println("Local host Status:"
+					+ EpzillaProcess.getInstance().getStatus());
+			
+//			//Now leader election complete. So no more election is running.
+//			EpzillaProcess.getInstance().setLeaderElectionRunning(false);
+
+			// Now create a Thread to count down for the pulse
+			// When PULSE received reset the count down
+		} else if (Integer.parseInt(strItems[0]) == MessageMeta.UID) {
+			// Initiate the Leader Election and Set the status to unknown
+			// Init the cluster leader
+			EpzillaProcess.getInstance().setClusterLeader(null);
+			EpzillaProcess.getInstance().setStatus(Status.UNKNOWN.toString());
+			EpzillaProcess.getInstance().setLeaderElectionRunning(true);
+			// Fire status changed event
+			EpzillaProcess.getInstance().fireEpzillaEvent(
+					new ProcessStatusChangedEvent());
+			// Forward the received UID to LCR and let it handle it.
+			LCRAlgoImpl.getInstance().runAlgorithm(message);
+
+		} else if (Integer.parseInt(strItems[0]) == MessageMeta.PULSE) {
+			// TODO TEST
+			System.out.println("PULSE Recieved from the Leader and the Pulse Received event fired.");
+			//Firing the pulse received event
+			EpzillaProcess.getInstance().fireEpzillaEvent(new PulseReceivedEvent(strItems[1]));
+			
+		}else if(Integer.parseInt(strItems[0])==MessageMeta.PING_LEADER){
+			System.out.println("PING received to this node and Ping Received event fired.");
+			//fire the ping received event. It will handle the logic of sending PULSE or REQUEST NOT ACCEPTED
+			EpzillaProcess.getInstance().fireEpzillaEvent(new PingReceivedEvent(strItems[1]));
+		}else if(Integer.parseInt(strItems[0])==MessageMeta.REQUEST_NOT_ACCEPTED){
+			System.out.println("REQUEST_NOT_ACCEPTED received from:"+strItems[1]);
+			//Whatever the last request to this host is not valid.
+			//Try to track the reason via message sequence from this side+response from other side
+			//Not implemented yet.
+			
+			System.out.println("Request rejected due to:"+RejectReason.getErrorMessage(Integer.parseInt(strItems[2])));
+			
+			EpzillaProcess.getInstance().fireEpzillaEvent(new RequestRejectedEvent(Integer.parseInt(strItems[2])));
+			
+		}
+
+	}
+
+}
