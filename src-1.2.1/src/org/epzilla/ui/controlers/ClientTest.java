56ef11a7e362280fad9570ee9bbe2654301a72c8
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-10 12:19:38 +0000

    updated

--- src-1.2.1/src/org/epzilla/ui/controlers/ClientTest.java
+++ src-1.2.1/src/org/epzilla/ui/controlers/ClientTest.java
@@ -12,6 +12,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.net.MalformedURLException;
 import java.util.TimerTask;
+import java.util.Timer;
 
 import jstm.core.Site;
 import jstm.core.Transaction;
@@ -24,8 +25,11 @@ import jstm.core.Transaction;
  * To change this template use File | Settings | File Templates.
  */
 public class ClientTest extends Thread{
-
+                    Object lock = new Object();
     DispInterface di;
+    static Thread trigger;
+    static Thread events;
+
     public ClientTest(){
 
     }
@@ -37,40 +41,102 @@ public class ClientTest extends Thread{
     public void initProcess(String ip, String name) throws MalformedURLException, NotBoundException, RemoteException {
         lookUp(ip,name);
         initSendTriggerStream();
-
+//        initEventsStream();
+        trigger.start();
+//        events.start();
     }
-   public void setDispatcherObj(Object obj){
+    public void initSendTriggerStream(){
+        trigger = new Thread(new Runnable() {
+            @Override
+        public void run(){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                while (true) {
+                synchronized (lock) {
+
+                    String str  = EventTriggerGenerator.getNextTrigger();
+                    String cID= "1";
+                    int triggerSeqID = 1;
+                    String response = null;
+
+                    byte []buffer =  str.getBytes();
+//                    System.out.println("trigger sending");
+
+
+                    try {
+                        response = di.uploadTriggersToDispatcher(buffer,cID,triggerSeqID);
+                    } catch (RemoteException e) {
+                    }
+
+                if(response!=null)
+                    System.out.println("Dispatcher Recieved the file from the client and the response is "+response);
+                else {
+                    System.out.println("File sending error reported.");
+                }
+}
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+                }
+            }
+        });
+    }
+  public void initEventsStream(){
+        final java.util.Timer timer1 = new java.util.Timer();
+        events = new Thread(new Runnable() {
+            @Override
+        public void run(){
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                while (true) {
+
+                 synchronized (lock){
+
+                String str  = EventTriggerGenerator.getNextTrigger();
+                String cID= "1";
+                int triggerSeqID = 1;
+                String response = null;
+
+                byte []buffer =  str.getBytes();
+                System.out.println("events manager");
+
+                try {
+                    response = di.uploadTriggersToDispatcher(buffer,cID,triggerSeqID);
+                } catch (RemoteException e) {
+                }
+
+            if(response!=null)
+                System.out.println("Dispatcher Recieved the file from the client and the response is "+response);
+            else {
+                System.out.println("File sending error reported.");
+            }
+                }
+            }
+            }
+        });
+    }
+    public void setDispatcherObj(Object obj){
         di = (DispInterface) obj;
    }
    public Object getDispatcherObj(){
         return di;
    }
-
-    public void initSendTriggerStream() {
-        final java.util.Timer timer1 = new java.util.Timer();
-        timer1.schedule(new TimerTask() {
-            @Override
-            public void run() {
-        String str  = EventTriggerGenerator.getNextTrigger();
-        String cID= "1";
-        int triggerSeqID = 1;
-       	String response = null;
-
-		byte []buffer =  str.getBytes();
-
-                try {
-                    response = di.uploadTriggersToDispatcher(buffer,cID,triggerSeqID);
-                } catch (RemoteException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                }
-                if(response!=null)
-        	System.out.println("Dispatcher Recieved the file from the client and the response is "+response);
-		else {
-			System.out.println("File sending error reported.");
-		}
-
-            }
-        }, 0, 5000);
-    }
-
+//     public static void main(String args[]) throws RemoteException {
+//       	 ClientTest test = new ClientTest();
+//         try {
+//             test.initProcess("10.8.108.151","Dispatcher");
+//         } catch (MalformedURLException e) {
+//             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//         } catch (NotBoundException e) {
+//             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//         }
+//     }
 }
