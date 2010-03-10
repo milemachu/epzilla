d3c9d8ab761bfe932c7b4a9d413745368c9932a0
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-03-10 18:53:03 +0000

    Basic bug fixes. Identified Problems: 1. Need different threads for message delivery and message decode. 2. Accessing epzillaProcess an be via synchronized method. 3. Listener registration 4. Use a Thread model which we used in the Treasure hunt so that listener always listens ans decodes messages via new thread. And that thread will make the necessary changes to the code. When we want to send a message use a different thread for that. Main thread will handle the Core but different threads for the message delivery and understanding them and taking actions.

---
+++ Leader/LCR1.1.2-Fixed-Broken[Maven]/src/org/epzilla/leader/LCRAlgoImpl.java
@@ -0,0 +1,162 @@
+package org.epzilla.leader;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Vector;
+
+import org.epzilla.leader.event.IEpzillaEventListner;
+import org.epzilla.leader.event.ProcessStatusChangedEvent;
+import org.epzilla.leader.message.MessageMeta;
+import org.epzilla.leader.rmi.LeaderMessageClient;
+import org.epzilla.leader.util.Status;
+
+/**
+ * This class contains the LCR leader election algorithm implementation. All the
+ * necessary LCR specific methods are here. Based on the assumption that 1 is
+ * the highest priority in UID sequence.
+ * 
+ * @author Administrator
+ * 
+ */
+public class LCRAlgoImpl {
+
+	// Private constructor prevents instantiation from other classes
+	private LCRAlgoImpl() {
+	}
+
+	/**
+	 * SingletonHolder is loaded on the first execution of
+	 * Singleton.getInstance() or the first access to SingletonHolder.INSTANCE,
+	 * not before.
+	 */
+	private static class LCRAlgoImplHolder {
+		private static final LCRAlgoImpl INSTANCE = new LCRAlgoImpl();
+	}
+
+	public static LCRAlgoImpl getInstance() {
+		return LCRAlgoImplHolder.INSTANCE;
+	}
+
+	// This is the LCR logic
+	public void runAlgorithm(String message) {
+		String[] strArray = message.split(Character
+				.toString(MessageMeta.SEPARATOR));
+
+		if (strArray != null && Integer.parseInt(strArray[1]) >= 1) {
+			int receivedUid = Integer.parseInt(strArray[1]);
+			if (receivedUid == EpzillaProcess.getInstance().getUID()) {
+				// This is the Leader. Send the Leader message to others
+				// UPDATE the local variables such as ClusterLeader and Status
+				try {
+
+					EpzillaProcess.getInstance().setClusterLeader(
+							InetAddress.getByName(strArray[2]));
+					EpzillaProcess.getInstance().setStatus(
+							Status.LEADER.toString());
+					EpzillaProcess.getInstance().fireEpzillaEvent(
+							new ProcessStatusChangedEvent());
+					EpzillaProcess.getInstance()
+							.setLeaderElectionRunning(false);
+
+					// Init epZilla Leader and ready to receive listeners
+					EpzillaLeader.getInstance().setClusterClientList(
+							new Vector<IEpzillaEventListner>());
+					System.out
+							.println("Cluster leader set as the localhost and Status changed event fired.");
+
+					// remove the hardcode address and get it from the ip
+					// list
+					try {
+						LeaderMessageClient.sendLeaderElectedMessage(EpzillaProcess
+								.getInstance().getIpNextToThis());
+					} catch (Exception e) {
+						boolean isMessageDelivered=false;
+						for (InetAddress nextIp : EpzillaProcess.getInstance().getClusterIpList()) {
+							if(!nextIp.getHostAddress().equalsIgnoreCase(InetAddress.getLocalHost().getHostAddress())){
+								try {
+									LeaderMessageClient.sendLeaderElectedMessage(nextIp.getHostAddress());
+									isMessageDelivered=true;
+									break;
+								} catch (Exception ex) {
+									continue;
+								}
+							}
+						}
+						
+						if(!isMessageDelivered){
+							LeaderMessageClient.sendLeaderElectedMessage(InetAddress.getLocalHost().getHostAddress());
+						}
+					}
+					
+					
+					
+					
+					
+					
+					System.out.println("Leader published as this host.");
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (receivedUid < EpzillaProcess.getInstance().getUID()) {
+				// Pass the message to next node
+				// If we want we can declare this as a non leader since this is
+				// going to be declared as a non leader upon the receiving the
+				// Leader published message.
+
+				try {
+					// remove hardcode and get from the ip list
+					LeaderMessageClient.forwardReceivedUidMessage(
+							EpzillaProcess.getInstance().getIpNextToThis(),
+							message);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (receivedUid > EpzillaProcess.getInstance().getUID()) {
+				// No Forwarding of received message.Instead start sending the
+				// UID of this
+				// Remove the hardcode and get it from the list
+				try {
+					LeaderMessageClient.sendUidMessage(EpzillaProcess
+							.getInstance().getIpNextToThis());
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NotBoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+}
---
+++ Leader/LCR1.1.2-Fixed-Broken[Maven]/src/org/epzilla/leader/event/EpzillaPulseSendTimer.java
@@ -0,0 +1,20 @@
+package org.epzilla.leader.event;
+
+import java.util.TimerTask;
+
+import org.epzilla.leader.EpzillaProcess;
+import org.epzilla.leader.util.Status;
+
+public class EpzillaPulseSendTimer extends TimerTask {
+
+	/**
+	 * This will fire the pulse timeout event in the epzilla process.
+	 */
+	public void run() {
+		if (EpzillaProcess.getInstance().getStatus().equalsIgnoreCase(
+				Status.LEADER.toString()))
+			EpzillaProcess.getInstance().fireEpzillaEvent(
+					new PulseIntervalTimeoutEvent());
+	}
+
+}
