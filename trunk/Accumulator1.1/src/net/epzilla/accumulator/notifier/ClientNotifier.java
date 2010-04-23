a9aafc5cb22f5dfbffa87304eb90acb89cde7111
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-04-23 18:59:39 +0000

    client notification

---
+++ trunk/Accumulator1.1/src/net/epzilla/accumulator/notifier/ClientNotifier.java
@@ -0,0 +1,14 @@
+package net.epzilla.accumulator.notifier;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chathura
+ * Date: Apr 24, 2010
+ * Time: 12:04:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClientNotifier {
+    public void sendNotification(String msg){
+        //send alert to the client
+    }
+}
