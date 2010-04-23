78eb29121267b2041e71bad945acee3867947873
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-04-23 18:59:39 +0000

    client notification

---
+++ Accumulator1.1/src/net/epzilla/accumulator/notifier/ClientManager.java
@@ -0,0 +1,16 @@
+package net.epzilla.accumulator.notifier;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chathura
+ * Date: Apr 23, 2010
+ * Time: 11:56:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClientManager {
+    public ClientManager(){
+    }
+    public void getClientIP(){
+        //get client ip from the Dispatcher / shared memory
+    }
+}
