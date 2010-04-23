d732f166ab1acf320e9b9ccc44c10a09b4513137
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-04-23 12:35:41 +0000

    Added customized implementation classes

---
+++ DynamicDiscovery/src/org/epzilla/common/discovery/accumulator/AccumulatorPublisher.java
@@ -0,0 +1,19 @@
+package org.epzilla.common.discovery.accumulator;
+
+import org.epzilla.common.discovery.IServicePublisher;
+
+public class AccumulatorPublisher implements IServicePublisher{
+
+	@Override
+	public void addSubscription(String serviceClient, String serviceName) {
+	}
+
+	@Override
+	public void publishService() {
+	}
+
+	@Override
+	public void removeSubscrition(String serviceClient, String serviceName) {
+	}
+
+}
