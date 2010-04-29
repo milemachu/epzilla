599e70a06893a4e0b3a72e1afe915ebea9ca0c43
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-04-29 14:00:55 +0000

    dynamic discovery part integration

---
+++ trunk/src-1.2.1/src/org/epzilla/common/discovery/Constants.java
@@ -0,0 +1,10 @@
+package org.epzilla.common.discovery;
+
+public interface Constants {
+	public static String MULTICAST_DELIMITER="\u0001";	
+	public static String TCP_UNICAST_DELIMITER="\u0002";
+	
+	public static String DISPATCHER_CLIENT_DELIMITER="\u0005";
+	public static String NODE_CLIENT_DELIMITER="\u0006";
+
+}
