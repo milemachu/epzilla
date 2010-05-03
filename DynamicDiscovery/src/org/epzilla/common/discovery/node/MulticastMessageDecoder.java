0c8df4260dd5ece4b5a130f1870c82c1e601c972
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-03 19:17:37 +0000

    NodeDiscoveryManager Complete and testing.

--- DynamicDiscovery/src/org/epzilla/common/discovery/node/MulticastMessageDecoder.java
+++ DynamicDiscovery/src/org/epzilla/common/discovery/node/MulticastMessageDecoder.java
@@ -6,6 +6,7 @@ import org.epzilla.common.discovery.unicast.TCPSender;
 public class MulticastMessageDecoder implements Runnable {
 
 	private String message;
+	private int tcpPort=5010;
 	
 	public MulticastMessageDecoder(String message) {
 	this.message=message;
@@ -34,10 +35,19 @@ public class MulticastMessageDecoder implements Runnable {
 				if(Integer.parseInt(info[1])==NodeDiscoveryManager.getClusterId()){
 					NodeDiscoveryManager.setClusterLeader(mcArr[1]);
 					//send a tcp msg to subscribe with it.
+					TCPSender ts=new TCPSender(mcArr[1], tcpPort);
+					ts.sendMessage("SUBSCRIBE_LEADER_SERVICE");
 				}
 			}
-		}else if(mcArr[0].equalsIgnoreCase("NODE_SERVICE")){
+		}else if(mcArr[0].startsWith("NODE_SERVICE")){
 			//message from another node. Add this to our node list.
+			//0-NODE_SERVICE;1-ClusterId
+			String []info=mcArr[0].split(Constants.CLUSTER_ID_DELIMITER);
+			if(Integer.parseInt(info[1])==NodeDiscoveryManager.getClusterId() && !((NodePublisher)NodeDiscoveryManager.getPublisher()).getNodes().contains(mcArr[1])){
+							//No msg. Just add update this side
+				((NodePublisher)NodeDiscoveryManager.getPublisher()).addSubscription(mcArr[1], "SUBSCRIBE_NODE_SERVICE");
+			
+			}
 		}
 	}
 
--- DynamicDiscovery/src/org/epzilla/common/discovery/node/NodeDiscoveryManager.java
+++ DynamicDiscovery/src/org/epzilla/common/discovery/node/NodeDiscoveryManager.java
@@ -1,7 +1,10 @@
 package org.epzilla.common.discovery.node;
 
 import org.epzilla.common.discovery.IServicePublisher;
+import org.epzilla.common.discovery.dispatcher.MulticastMessageDecoder;
+import org.epzilla.common.discovery.dispatcher.TCPMessageDecoder;
 import org.epzilla.common.discovery.multicast.MulticastReceiver;
+import org.epzilla.common.discovery.unicast.TCPListener;
 
 public class NodeDiscoveryManager {
 	
@@ -19,26 +22,73 @@ public class NodeDiscoveryManager {
 	
 	
 
-	public NodeDiscoveryManager(int clusterId) {
+	public NodeDiscoveryManager(final int clusterId) {
 	
 		NodeDiscoveryManager.clusterId=clusterId;
 		
 		leaderPublisher=new LeaderPublisher();
 		nodePublisher=new NodePublisher();
 		
+		tcpListenerThread=new Thread(new Runnable() {
+			TCPListener tcpListner;
+			@Override
+			public void run() {
+				tcpListner=new TCPListener(tcpPort); 
+				
+				while (true) {
+					String messageReceived=tcpListner.MessageReceived();
+					Thread executor=new Thread(new TCPMessageDecoder(messageReceived));
+					executor.start();
+				}
+			}
+		});
+		
+		tcpListenerThread.start();
+		
 		multicastListnerThread=new Thread(new Runnable() {
-			@SuppressWarnings("unused")
 			MulticastReceiver mcReceiver;
 			@Override
 			public void run() {
 				mcReceiver=new MulticastReceiver(multicastGroupIp, multicastPort);
 				
 				while (true) {
+					String messageReceived=mcReceiver.messageReceived();
+					Thread executor=new Thread(new MulticastMessageDecoder(messageReceived));
+					executor.start();
+				}
+			}
+		});
+		
+		multicastListnerThread.start();
+		
+//Now Broadcast out capabilities via publisher.
+		
+		multicastSenderThread=new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					nodePublisher.publishService(clusterId);
+					
+					if(isLeader()){
+						leaderPublisher.publishService(clusterId);
+					}
+					
 					
 				}
 			}
 		});
+	
+		multicastSenderThread.start();
 	}
+	
+	
 	
 	public static IServicePublisher getPublisher(){
 		if(isLeadeer)
@@ -67,4 +117,9 @@ public class NodeDiscoveryManager {
 		NodeDiscoveryManager.clusterLeader=clusterLeader;
 	}
 	
+	
+	public static void main(String[] args) {
+		NodeDiscoveryManager nodeMan=new NodeDiscoveryManager(5);
+		
+	}
 }
