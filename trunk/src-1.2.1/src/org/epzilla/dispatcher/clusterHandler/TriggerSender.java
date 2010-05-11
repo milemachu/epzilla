ca6e838c8c99b366d234faac9d3fe7a0714b0542
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-11 18:07:43 +0000

    update trigger sender class

--- trunk/src-1.2.1/src/org/epzilla/dispatcher/clusterHandler/TriggerSender.java
+++ trunk/src-1.2.1/src/org/epzilla/dispatcher/clusterHandler/TriggerSender.java
@@ -7,6 +7,7 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,6 +17,7 @@ import java.util.ArrayList;
  * To change this template use File | Settings | File Templates.
  */
 public class TriggerSender {
+    private static HashMap idMap = new HashMap<String, String>();
     private static ClusterInterface clusterObj;
     private static String response = null;
 
@@ -23,23 +25,29 @@ public class TriggerSender {
     }
 
     public static void acceptTrigger(String serverIp, String clusterID, ArrayList<String> triggers, String clientID) throws MalformedURLException, NotBoundException, RemoteException {
-        initCluster(serverIp, "CLUSTER_NODE");
-        sendTriggers(triggers, clusterID, clientID);
+        if (idMap.containsKey(serverIp)) {
+            clusterObj = (ClusterInterface) idMap.get(serverIp);
+            sendTriggers(triggers, clusterID, clientID);
+        } else {
+            initCluster(serverIp, "CLUSTER_NODE");
+            sendTriggers(triggers, clusterID, clientID);
+        }
+
     }
 
     private static void initCluster(String serverIp, String serviceName) throws MalformedURLException, NotBoundException, RemoteException {
         String url = "rmi://" + serverIp + "/" + serviceName;
         ClusterInterface obj = (ClusterInterface) Naming.lookup(url);
         setClusterObject(obj);
+        idMap.put(serverIp,clusterObj);
 
     }
 
     private static void sendTriggers(ArrayList<String> triggers, String clusterID, String clientID) throws RemoteException, MalformedURLException, NotBoundException {
         response = clusterObj.acceptTiggerStream(triggers, clusterID, clientID);
-        if (response != null){
+        if (response != null) {
             System.out.println("Triggers send to the cluster");
-        }
-        else{
+        } else {
             System.out.println("Triggers not accepted");
         }
     }
