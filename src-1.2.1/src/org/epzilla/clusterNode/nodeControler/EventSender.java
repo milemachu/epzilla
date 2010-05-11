ba2528e082dd336710ebd075bfa1757eef64e043
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-11 03:32:42 +0000

    Event, Trigger sending mechanism added to the clusters. Partially implemented.

--- src-1.2.1/src/org/epzilla/clusterNode/nodeControler/EventSender.java
+++ src-1.2.1/src/org/epzilla/clusterNode/nodeControler/EventSender.java
@@ -1,5 +1,11 @@
 package org.epzilla.clusterNode.nodeControler;
 
+import org.epzilla.clusterNode.rmi.ClusterInterface;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 /**
@@ -10,8 +16,42 @@ import java.util.ArrayList;
  * To change this template use File | Settings | File Templates.
  */
 public class EventSender {
-    public EventSender(){}
-     public void sendEvents(String ip, String clientID, ArrayList<String> eventStream) {
+    private static ClusterInterface clusterObj;
+    private static String response = null;
+    private static String nodeIP, clientID;
+    private static ArrayList<String> events;
+
+    public EventSender() {
+    }
+
+    public EventSender(String ip, String clientID, ArrayList<String> eventStream) {
+        this.nodeIP = ip;
+        this.clientID = clientID;
+        this.events = eventStream;
 
     }
+
+    public static void sendEvents() throws RemoteException, MalformedURLException, NotBoundException {
+        initNode(nodeIP, "CLUSTER_NODE");
+
+        response = clusterObj.addEventStream(events, clientID);
+
+        if (response != null) {
+            System.out.println("Events added to the Node " + nodeIP);
+        } else {
+            System.err.println("Events adding failure to the Node" + nodeIP);
+        }
+    }
+
+    private static void initNode(String serverIp, String serviceName) throws MalformedURLException, NotBoundException, RemoteException {
+        String url = "rmi://" + serverIp + "/" + serviceName;
+        ClusterInterface obj = (ClusterInterface) Naming.lookup(url);
+        setClusterObject(obj);
+
+    }
+
+    private static void setClusterObject(Object obj) {
+        clusterObj = (ClusterInterface) obj;
+    }
+
 }
--- src-1.2.1/src/org/epzilla/clusterNode/rmi/ClusterInterface.java
+++ src-1.2.1/src/org/epzilla/clusterNode/rmi/ClusterInterface.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
  * Created by IntelliJ IDEA.
  * User: Team epZilla
  * Date: Mar 11, 2010
+ * Modified: May 11, 2010
  * Time: 10:44:48 AM
  * To change this template use File | Settings | File Templates.
  */
@@ -16,6 +17,10 @@ public interface ClusterInterface extends Remote {
     public String acceptTiggerStream(ArrayList<String> tList, String clusterID, String clientID) throws RemoteException;
 
     public String acceptEventStream(ArrayList<String> eList, String clusterID, String clientID) throws RemoteException;
+
+    public String addEventStream(ArrayList<String> eList, String clientID) throws RemoteException;
+
+    public String addTriggerStream(ArrayList<String> tlist, String clientID) throws RemoteException;
 
     public String deleteTriggers(ArrayList<String> list, String clusterID, String clientID) throws RemoteException;
 }
