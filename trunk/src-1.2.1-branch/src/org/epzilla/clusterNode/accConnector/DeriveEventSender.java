8014e8245a2fad020d46a8a71347ddf2efc5ec05
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-19 13:58:46 +0000

    derive event sender [initiated]

---
+++ trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/accConnector/DeriveEventSender.java
@@ -0,0 +1,11 @@
+package org.epzilla.clusterNode.accConnector;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Randika
+ * Date: May 15, 2010
+ * Time: 7:15:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeriveEventSender {
+}
