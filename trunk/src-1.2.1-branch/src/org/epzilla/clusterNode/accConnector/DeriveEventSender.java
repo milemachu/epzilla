3fa2323797eac8e727508957dfdb9dfda6dc6052
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-19 15:18:23 +0000

    Derive event sender completed, Need logic to Derive Event Manager

--- trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/accConnector/DeriveEventSender.java
+++ trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/accConnector/DeriveEventSender.java
@@ -1,5 +1,13 @@
 package org.epzilla.clusterNode.accConnector;
 
+import org.epzilla.accumulator.service.AccumulatorService;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Hashtable;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Randika
@@ -8,4 +16,38 @@ package org.epzilla.clusterNode.accConnector;
  * To change this template use File | Settings | File Templates.
  */
 public class DeriveEventSender {
+    private static AccumulatorService accObj;
+    private static String response = null;
+    private static Hashtable<String, Object> accList = new Hashtable<String, Object>();
+
+    public DeriveEventSender() {
+    }
+
+    public static void sendDeriveEvent(String serverIP, byte[] deriveEvent) throws MalformedURLException, NotBoundException, RemoteException {
+
+        if (!accList.containsKey(serverIP)) {
+            initAccumulator(serverIP, "ACCUMULATOR_SERVICE");
+            accObj.receiveDeriveEvent(deriveEvent);
+        } else {
+            accObj = (AccumulatorService) accList.get(serverIP);
+            accObj.receiveDeriveEvent(deriveEvent);
+        }
+
+    }
+
+    private static void initAccumulator(String serverIP, String serviceName) throws MalformedURLException, NotBoundException, RemoteException {
+        String url = "rmi://" + serverIP + "/" + serviceName;
+        AccumulatorService obj = (AccumulatorService) Naming.lookup(url);
+        setAccObject(obj);
+        accList.put(serverIP, getAccObject());
+
+    }
+
+    private static void setAccObject(Object obj) {
+        accObj = (AccumulatorService) obj;
+    }
+
+    private static Object getAccObject() {
+        return accObj;
+    }
 }
