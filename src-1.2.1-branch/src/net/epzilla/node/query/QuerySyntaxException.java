b4c4e35ed14bd97e974a59f7aaf14738e0fe8ee1
rjvranga <rjvranga@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-24 07:48:30 +0000

    executors.

---
+++ src-1.2.1-branch/src/net/epzilla/node/query/QuerySyntaxException.java
@@ -0,0 +1,11 @@
+package net.epzilla.node.query;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajeev
+ * Date: Mar 8, 2010
+ * Time: 8:46:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QuerySyntaxException extends Exception {
+}
