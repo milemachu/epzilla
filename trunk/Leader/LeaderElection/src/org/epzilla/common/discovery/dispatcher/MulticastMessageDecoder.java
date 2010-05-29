3029ece4854bfce170f813547cdff8a08f419fb5
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-29 21:13:29 +0000

    Leader Election complete for Dispatchers as well. Only Timer Service to Implement and test.

--- trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/dispatcher/MulticastMessageDecoder.java
+++ trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/dispatcher/MulticastMessageDecoder.java
@@ -17,12 +17,11 @@ public class MulticastMessageDecoder implements Runnable {
 		String []mcArr=message.split(Constants.MULTICAST_DELIMITER);
 		if(mcArr[0].equalsIgnoreCase("DISPATCHER_SERVICE") && !DispatcherDiscoveryManager.getDispatcherPublisher().getDispatchers().contains(mcArr[1])){
 			DispatcherDiscoveryManager.getDispatcherPublisher().insertDispatcher(mcArr[1]);
-		}else if(mcArr[0].equalsIgnoreCase("DISPATCHER_LEADER_SERVICE") && DispatcherDiscoveryManager.getDispatcherLeader()==null && !DispatcherDiscoveryManager.isLeader()){
+		}else if(mcArr[0].equalsIgnoreCase("DISPATCHER_LEADER_SERVICE") && DispatcherDiscoveryManager.getDispatcherLeader()==null){
 			DispatcherDiscoveryManager.setDispatcherLeader(mcArr[1]);
 			//send a tcp msg to subscribe with it.
 			TCPSender ts=new TCPSender(mcArr[1], tcpPort);
 			ts.sendMessage("SUBSCRIBE_DISPATCHER_LEADER_SERVICE");
 		}
 	}
-
 }
