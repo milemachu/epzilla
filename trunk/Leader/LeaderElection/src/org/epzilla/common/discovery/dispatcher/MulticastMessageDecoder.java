94c3ab67bc520046743cab21aa71384be0f1982b
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-30 18:56:28 +0000

    Fixed the issue of Dispatchers who participated in LE not subscribing with Leader [Fixed]

--- trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/dispatcher/MulticastMessageDecoder.java
+++ trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/dispatcher/MulticastMessageDecoder.java
@@ -17,8 +17,9 @@ public class MulticastMessageDecoder implements Runnable {
 		String []mcArr=message.split(Constants.MULTICAST_DELIMITER);
 		if(mcArr[0].equalsIgnoreCase("DISPATCHER_SERVICE") && !DispatcherDiscoveryManager.getDispatcherPublisher().getDispatchers().contains(mcArr[1])){
 			DispatcherDiscoveryManager.getDispatcherPublisher().insertDispatcher(mcArr[1]);
-		}else if(mcArr[0].equalsIgnoreCase("DISPATCHER_LEADER_SERVICE") && DispatcherDiscoveryManager.getDispatcherLeader()==null){
+		}else if(mcArr[0].equalsIgnoreCase("DISPATCHER_LEADER_SERVICE") && !DispatcherDiscoveryManager.isSubscribedWithLeader() && !DispatcherDiscoveryManager.isLeader()){
 			DispatcherDiscoveryManager.setDispatcherLeader(mcArr[1]);
+			DispatcherDiscoveryManager.setSubscribedWithLeader(true);
 			//send a tcp msg to subscribe with it.
 			TCPSender ts=new TCPSender(mcArr[1], tcpPort);
 			ts.sendMessage("SUBSCRIBE_DISPATCHER_LEADER_SERVICE");
