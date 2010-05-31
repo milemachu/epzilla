00f43b015467593c971ddc0dbc5daab619f43f7e
rjvranga <rjvranga@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-05-31 08:27:46 +0000

    table size fixed.

--- src-1.2.1-branch/src/org/epzilla/dispatcher/ui/EpzillaTable.java
+++ src-1.2.1-branch/src/org/epzilla/dispatcher/ui/EpzillaTable.java
@@ -1,19 +1,31 @@
 package org.epzilla.dispatcher.ui;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import java.util.Enumeration;
 
 public class EpzillaTable extends JPanel {
 
     public EpzillaTable() {
         EpzillaDataModel mdm = new EpzillaDataModel();
-
+        this.setLayout(new CustomGridLayout(new String[] {"100%"}, new String[] {"100%"}));
         JTable jt = new JTable(mdm);
         jt.setModel(mdm);
-        jt.setFillsViewportHeight(true);
 //        jt.setBounds(new Rectangle(713, 340, 281, 170));
 
         JScrollPane jsp = new JScrollPane(jt);
+        jt.setFillsViewportHeight(true);
+//        jt.setBounds(0,0,300,90);
+
+//        TableColumnModel tcm = jt.getColumnModel();
+//        Enumeration e = tcm.getColumns();
+//        while (e.hasMoreElements()) {
+//            TableColumn tc = (TableColumn) e.nextElement();
+//            tc.setMinWidth(80);
+//            tc.setPreferredWidth(80);
+//            tc.setMaxWidth(80);
+//        }
 //        jsp.setBounds(new Rectangle(713, 340, 281, 170));
 
         super.add(jsp);
