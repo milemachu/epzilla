83b2068f2cb59238f3af23b75d11dcea4159aeb9
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-01 06:38:53 +0000

    fixed some bugs and update some parts

--- trunk/src-1.2.1-branch/src/org/epzilla/accumulator/notificationSys/ClientNotifier.java
+++ trunk/src-1.2.1-branch/src/org/epzilla/accumulator/notificationSys/ClientNotifier.java
@@ -7,7 +7,9 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,7 +23,7 @@ public class ClientNotifier {
     private static String response = null;
     private static StringBuilder cIP;
     private static String clientIP;
-
+    private static ArrayList<String> alertBuffer = new ArrayList<String>();
 
     public ClientNotifier() {
 
@@ -37,6 +39,7 @@ public class ClientNotifier {
                 Logger.log("Notifications send to the client");
                 NotificationManager.setAlertCount();
             } else {
+                alertBuffer.add(alerts + ":" + clientID);
                 Logger.log("Notifications not sent");
             }
         } else {
@@ -47,11 +50,20 @@ public class ClientNotifier {
                 Logger.log("Notifications send to the client");
                 NotificationManager.setAlertCount();
             } else {
+                alertBuffer.add(alerts + ":" + clientID);
                 Logger.log("Notifications not sent");
             }
         }
     }
 
+    public static void resendAlerts() throws MalformedURLException, NotBoundException, RemoteException {
+        for (String buffer : alertBuffer) {
+            StringTokenizer st = new StringTokenizer(buffer, ":");
+            String alert = st.nextToken();
+            String id = st.nextToken();
+            sendAlertMsg(alert, id);
+        }
+    }
 
     private static ClientInterface initClient(String clientID, String serverIp, String serviceName) throws MalformedURLException, NotBoundException, RemoteException {
         String url = "rmi://" + serverIp + "/" + serviceName;
@@ -69,7 +81,7 @@ public class ClientNotifier {
         String preCharacter = "";
         int period = 3;
         int index = 0;
-
+        String clientIp = "";
         cIP = new StringBuilder(cid.length() + toInsert.length() * (cid.length() / period) + 1);
         while (index < cid.length()) {
             cIP.append(preCharacter);
@@ -77,7 +89,27 @@ public class ClientNotifier {
             cIP.append(cid.substring(index, Math.min(index + period, cid.length())));
             index += period;
         }
-        return clientIP.toString();
+        clientIp = cIP.toString();
+        return clientIp;
     }
+    //method for testing the notification system
 
+    public static void main(String[] args) {
+        try {
+            for (int i = 0; i < 10; i++) {
+                sendAlertMsg("hello", "127000000001");
+                Thread.sleep(2000);
+            }
+            resendAlerts();
+        } catch (RemoteException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (MalformedURLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (NotBoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 }
