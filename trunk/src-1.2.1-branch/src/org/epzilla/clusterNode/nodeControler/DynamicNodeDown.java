4c4e5b711ec95b6eb9b34d74c1c05261d7b3bd0b
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-10 15:27:59 +0000

    dynamically shut down a node

---
+++ trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/nodeControler/DynamicNodeDown.java
@@ -0,0 +1,19 @@
+package org.epzilla.clusterNode.nodeControler;
+
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chathura
+ * Date: Jun 10, 2010
+ * Time: 7:42:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DynamicNodeDown {
+
+    public static void shutDown() throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("shutdown -s -t 0");
+        System.exit(0);
+    }
+}
