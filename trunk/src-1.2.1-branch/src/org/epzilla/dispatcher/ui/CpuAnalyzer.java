689bbd0f971e330db528c1da00af085993833a65
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-14 15:32:14 +0000

    cpu analyzer added

---
+++ trunk/src-1.2.1-branch/src/org/epzilla/dispatcher/ui/CpuAnalyzer.java
@@ -0,0 +1,181 @@
+package org.epzilla.dispatcher.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.TimerTask;
+
+import static java.awt.Color.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chathura
+ * Date: Jun 14, 2010
+ * Time: 8:20:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CpuAnalyzer extends JPanel {
+
+    public CpuAnalyzer() {
+        setLayout(new BorderLayout());
+        DisplayPanel displayPanel = new DisplayPanel();
+        add(displayPanel);
+    }
+
+    public class DisplayPanel extends JPanel {
+
+        public Thread runner = null;
+        public long time = 500;
+        private int w, h;
+        private int cInc;
+        private int points[];
+        private int nums;
+        private int aH, dH;
+        private String name;
+        private BufferedImage image;
+        private Graphics2D graphics;
+        private Font myFont = new Font("Calibri", Font.PLAIN, 11);
+        private Runtime runtime = Runtime.getRuntime();
+        private Rectangle rect1 = new Rectangle();
+        private Rectangle2D rect2 = new Rectangle2D.Float();
+        private Rectangle2D rect3 = new Rectangle2D.Float();
+        private Line2D gLine = new Line2D.Float();
+        private Color gColor = new Color(46, 139, 87);
+        private Color plotColor = new Color(0, 100, 0);
+        java.util.Timer timer = new java.util.Timer();
+
+        public DisplayPanel() {
+            setBackground(BLACK);
+            if (runner == null)
+                initProcess();
+        }
+
+        public void initProcess() {
+            timer.scheduleAtFixedRate(new TimerTask() {
+                public void run() {
+
+                    Dimension d = getSize();
+                    if (d.width != w || d.height != h) {
+                        w = d.width;
+                        h = d.height;
+                        image = (BufferedImage) createImage(w, h);
+                        graphics = image.createGraphics();
+                        graphics.setFont(myFont);
+                        FontMetrics fontMatrics = graphics.getFontMetrics(myFont);
+                        aH = fontMatrics.getAscent();
+                        dH = fontMatrics.getDescent();
+                    }
+                    repaint();
+                }
+            }, 10, 500);
+        }
+
+        public void paint(Graphics g) {
+
+            if (graphics == null) {
+                return;
+            }
+
+            graphics.setBackground(getBackground());
+            graphics.clearRect(0, 0, w, h);
+
+            //get the CPU usage of the machine
+            float freeMemory = (float) runtime.freeMemory();
+            float totalMemory = (float) runtime.totalMemory();
+
+            graphics.setColor(GREEN);
+//            graphics.drawString(String.valueOf((int) totalMemory / 1024) + "K allocated", 4.0f, (float) aH + 0.5f);
+            name = String.valueOf(((int) (totalMemory - freeMemory)) / 100)
+                    + " % ";
+            graphics.drawString(name, 4, h - dH);
+
+            float ssH = aH + dH;
+            float remainingHeight = h - (ssH * 2) - 0.5f;
+            float blockHeight = remainingHeight / 10;
+            float blockWidth = 20.0f;
+
+            graphics.setColor(plotColor);
+            int MemUsage = (int) ((freeMemory / totalMemory) * 10);
+            int i = 0;
+            for (; i < MemUsage; i++) {
+                rect2.setRect(5, ssH + i * blockHeight,
+                        blockWidth, blockHeight - 1);
+                graphics.fill(rect2);
+            }
+
+            graphics.setColor(GREEN);
+            for (; i < 10; i++) {
+                rect3.setRect(5, ssH + i * blockHeight,
+                        blockWidth, blockHeight - 1);
+                graphics.fill(rect3);
+            }
+
+            graphics.setColor(gColor);
+            int graphX = 30;
+            int graphY = (int) ssH;
+            int graphW = w - graphX - 5;
+            int graphH = (int) remainingHeight;
+            rect1.setRect(graphX, graphY, graphW, graphH);
+            graphics.draw(rect1);
+
+            int graphRow = graphH / 10;
+
+
+            for (int j = graphY; j <= graphH + graphY; j += graphRow) {
+                gLine.setLine(graphX, j, graphX + graphW, j);
+                graphics.draw(gLine);
+            }
+
+            int graphColumn = graphW / 15;
+
+            if (cInc == 0) {
+                cInc = graphColumn;
+            }
+
+            for (int j = graphX + cInc; j < graphW + graphX; j += graphColumn) {
+                gLine.setLine(j, graphY, j, graphY + graphH);
+                graphics.draw(gLine);
+            }
+
+            --cInc;
+
+            if (points == null) {
+                points = new int[graphW];
+                nums = 0;
+            } else if (points.length != graphW) {
+                int tmp[];
+                if (nums < graphW) {
+                    tmp = new int[nums];
+                    System.arraycopy(points, 0, tmp, 0, tmp.length);
+                } else {
+                    tmp = new int[graphW];
+                    System.arraycopy(points, points.length - tmp.length, tmp, 0, tmp.length);
+                    nums = tmp.length - 2;
+                }
+                points = new int[graphW];
+                System.arraycopy(tmp, 0, points, 0, tmp.length);
+            } else {
+                graphics.setColor(YELLOW);
+                points[nums] = (int) (graphY + graphH * (freeMemory / totalMemory));
+                for (int j = graphX + graphW - nums, k = 0; k < nums; k++, j++) {
+                    if (k != 0) {
+                        if (points[k] != points[k - 1]) {
+                            graphics.drawLine(j - 1, points[k - 1], j, points[k]);
+                        } else {
+                            graphics.fillRect(j, points[k], 1, 1);
+                        }
+                    }
+                }
+                if (nums + 2 == points.length) {
+                    System.arraycopy(points, 1, points, 0, nums - 1);
+                    --nums;
+                } else {
+                    nums++;
+                }
+            }
+            g.drawImage(image, 0, 0, this);
+        }
+    }
+}
