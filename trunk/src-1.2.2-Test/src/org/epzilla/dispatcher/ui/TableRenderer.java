d116d66d6584030a527974434b85b57e89e622ac
rjvranga <rjvranga@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-15 05:29:31 +0000

    table renderer.

--- trunk/src-1.2.2-Test/src/org/epzilla/dispatcher/ui/TableRenderer.java
+++ trunk/src-1.2.2-Test/src/org/epzilla/dispatcher/ui/TableRenderer.java
@@ -19,8 +19,8 @@ public class TableRenderer implements TableCellRenderer {
         final String txt = value.toString();
 
         JLabel jl = null;
-        final  String colname = table.getColumnName(column);
-        
+        final String colname = table.getColumnName(column);
+
         try {
             if (colname.equals(EpzillaDataModel.headers[EpzillaDataModel.CPU]) || colname.equals(EpzillaDataModel.headers[EpzillaDataModel.MEMORY])) {
                 final int i = (Integer) value;
@@ -40,19 +40,26 @@ public class TableRenderer implements TableCellRenderer {
                     protected void paintComponent(Graphics g) {
                         super.paintComponent(g);    //To change body of overridden methods use File | Settings | File Templates.
                         g.setColor(fc);
-                        g.fillRect(0, 0, (int) ((this.getWidth()) * (i / 100.0d)), this.getHeight());
+                        int ww = (int) ((this.getWidth()) * (i / 100.0d));
+                        g.fillRect(0, 0, ww, this.getHeight());
+                        g.setColor(Color.black);
+                        g.fillRect(ww, 0, this.getWidth() - ww, this.getHeight());
                         g.setColor(Color.black);
                         g.drawString(txt, 2, 12);
+
+
                     }
                 };
 
 //                 jl.setBackground(back);
             } else {
-                  jl = new JLabel() {
+                jl = new JLabel() {
                     @Override
                     protected void paintComponent(Graphics g) {
                         super.paintComponent(g);    //To change body of overridden methods use File | Settings | File Templates.
                         g.setColor(Color.black);
+                        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+                        g.setColor(Color.white);
                         g.drawString(txt, 3, 12);
                     }
                 };
