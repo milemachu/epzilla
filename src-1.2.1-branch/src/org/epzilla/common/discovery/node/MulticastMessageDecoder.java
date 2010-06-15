c615834027d0cd05837f5310e27d0c70f2f8f578
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-15 09:24:39 +0000

    Latest Code

---
+++ src-1.2.1-branch/src/org/epzilla/common/discovery/node/MulticastMessageDecoder.java
@@ -0,0 +1,56 @@
+package org.epzilla.common.discovery.node;
+
+import org.epzilla.common.discovery.Constants;
+import org.epzilla.common.discovery.unicast.TCPSender;
+
+public class MulticastMessageDecoder implements Runnable {
+
+	private String message;
+	private int tcpPort=5010;
+	
+	public MulticastMessageDecoder(String message) {
+	this.message=message;
+	}
+	
+	public void run() {
+		//0=message;1=multicastSender
+		String []mcArr=message.split(Constants.MULTICAST_DELIMITER);
+		if(mcArr[0].equalsIgnoreCase("DISPATCHER_SERVICE")){
+			//If this is the leader subscribe. else forget it.
+			//Send TCP connections to them.
+			if(NodeDiscoveryManager.isLeader() && !NodeDiscoveryManager.getLeaderPublisher().getDispatchers().contains(mcArr[1])){
+				TCPSender ts=new TCPSender(mcArr[1], 5010);
+				ts.sendMessage(NodeDiscoveryManager.getClusterId()+Constants.DISPATCHER_CLIENT_DELIMITER+"SUBSCRIBE_DISPATCHER_SERVICE");
+				
+				//Now update the dispatcher list in the leader service.
+				NodeDiscoveryManager.getLeaderPublisher().updateDispatcherList(mcArr[1]);
+			}
+		}else if(mcArr[0].startsWith("LEADER_SERVICE")){
+			//if this is a node client subscribe it.else forget it.
+			//Send the TCP connection.
+			if(!NodeDiscoveryManager.isLeader()){
+				//Wait till the Leader publisher imple finishes.
+				//0-LEADER_SERVICE;1-ClusterId
+				String []info=mcArr[0].split(Constants.CLUSTER_ID_DELIMITER);
+				if(Integer.parseInt(info[1])==NodeDiscoveryManager.getClusterId() && !NodeDiscoveryManager.isSubscribedWithLeader()){
+					NodeDiscoveryManager.setClusterLeader(mcArr[1]);
+					NodeDiscoveryManager.setSubscribedWithLeader(true);
+					
+					//send a tcp msg to subscribe with it.
+					TCPSender ts=new TCPSender(mcArr[1], tcpPort);
+					ts.sendMessage("SUBSCRIBE_LEADER_SERVICE");
+				}
+			}
+		}else if(mcArr[0].startsWith("NODE_SERVICE")){
+			//message from another node. Add this to our node list.
+			//0-NODE_SERVICE;1-ClusterId
+			String []info=mcArr[0].split(Constants.CLUSTER_ID_DELIMITER);
+			if(Integer.parseInt(info[1])==NodeDiscoveryManager.getClusterId() && !NodeDiscoveryManager.getNodePublisher().getNodes().contains(mcArr[1])){
+							//No msg. Just add update this side
+				NodeDiscoveryManager.getNodePublisher().addSubscription(mcArr[1], "SUBSCRIBE_NODE_SERVICE");
+			
+			}
+		}
+	}
+
+}
---
+++ src-1.2.1-branch/src/org/epzilla/leader/service/NodeUpdateService.java
@@ -0,0 +1,129 @@
+package org.epzilla.leader.service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import org.epzilla.common.discovery.node.NodeDiscoveryManager;
+import org.epzilla.leader.Epzilla;
+import org.epzilla.leader.client.NodeClientManager;
+import org.epzilla.leader.message.RmiMessageClient;
+import org.epzilla.leader.util.Status;
+import org.epzilla.leader.util.SystemConstants;
+
+public class NodeUpdateService extends Thread implements IEpzillaService{
+	// Private constructor prevents instantiation from other classes
+	 private NodeUpdateService() {
+		 this.setDaemon(true);
+	 }
+	   
+	 public void  run() {
+		while (true) {
+			try {
+				Thread.sleep(SystemConstants.UPDATE_SERVICE_RUNNING_TIME);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			executeService();
+		}
+	   }
+	 
+	   /**
+	    * SingletonHolder is loaded on the first execution of Singleton.getInstance() 
+	    * or the first access to SingletonHolder.INSTANCE, not before.
+	    */
+	   private static class NodeUpdateServiceHolder { 
+	     private static final NodeUpdateService INSTANCE = new NodeUpdateService();
+	   }
+	 
+	   public static NodeUpdateService getInstance() {
+	     return NodeUpdateServiceHolder.INSTANCE;
+	   }
+	
+	
+
+	@SuppressWarnings("unchecked")
+	public void executeService() {
+		
+		HashSet<String> currentNodeList=new HashSet<String>(NodeClientManager.getNodeList());
+		HashSet<String> respondedList=new HashSet<String>();
+		try {
+			currentNodeList.remove(InetAddress.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		for (Iterator iterator = currentNodeList.iterator(); iterator.hasNext();) {
+			final String string = (String) iterator.next();
+			String response=RmiMessageClient.getStateFromRemote(string);
+			if(response!=null){
+				respondedList.add(string);
+			}			
+		}
+		
+		if(NodeClientManager.getNodeList().size()==currentNodeList.size()+1){
+			//No new updates to node list
+			currentNodeList.removeAll(respondedList);
+			for (Iterator iterator = currentNodeList.iterator(); iterator
+					.hasNext();) {
+				String string = (String) iterator.next();
+				NodeDiscoveryManager.getNodePublisher().removeSubscrition(string, "UNSUBSCRIBE_NODE_SERVICE");
+				System.out.println("Removed a dead node from node list :"+string +"Removed by "+Thread.currentThread().getId());
+			}
+			
+		}
+		
+		if(Epzilla.getStatus().equalsIgnoreCase(Status.LEADER.name()))
+			executeLeaderTasks(respondedList);
+		
+		currentNodeList=null;
+		respondedList=null;
+		System.gc();				
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void executeLeaderTasks(HashSet<String> respondedList){
+		HashSet<String> currentSubscribedNodeList=new HashSet<String>(NodeClientManager.getSubscribedNodeList());
+		HashSet<String> currentDispatcherList=new HashSet<String>(NodeClientManager.getDispatcherList());
+		HashSet<String> respondedDispatcherList=new HashSet<String>();
+		
+		if(NodeClientManager.getSubscribedNodeList().size()==currentSubscribedNodeList.size()){
+			//No new updated for subscribed list
+			currentSubscribedNodeList.removeAll(respondedList);
+			for (Iterator iterator = currentSubscribedNodeList.iterator(); iterator
+					.hasNext();) {
+				String string = (String) iterator.next();
+				NodeDiscoveryManager.getLeaderPublisher().removeSubscrition(string, "UNSUBSCRIBE_LEADER_SERVICE");
+				System.out.println("Removed a dead node from subscribers :"+string+"Removed by "+Thread.currentThread().getId());
+				
+			}
+		}
+		
+		for (Iterator iterator = currentDispatcherList.iterator(); iterator.hasNext();) {
+			final String string = (String) iterator.next();
+			String response=RmiMessageClient.getStateFromRemote(string);
+			if(response!=null){
+				respondedDispatcherList.add(string);
+			}			
+		}
+		
+		if(NodeClientManager.getDispatcherList().size()==currentDispatcherList.size()){
+			//No new dispatcher has arrived.
+			currentDispatcherList.removeAll(respondedDispatcherList);
+			for (Iterator iterator = currentDispatcherList.iterator(); iterator
+					.hasNext();) {
+				String string = (String) iterator.next();
+				NodeDiscoveryManager.getLeaderPublisher().removeDispatcherList(string);
+				System.out.println("Removed a dispatcher from Dispatcher list :"+string);
+				
+			}
+		}
+		
+		currentSubscribedNodeList=null;
+		currentDispatcherList=null;
+		respondedDispatcherList=null;
+		respondedList=null;
+	}
+
+}
