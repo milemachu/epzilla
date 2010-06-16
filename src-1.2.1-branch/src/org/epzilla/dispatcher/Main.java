9e9f6d60b9d7c2fb26073ec88a60140916ce7343
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-16 12:10:23 +0000

    new main class for the dispatcher

---
+++ src-1.2.1-branch/src/org/epzilla/dispatcher/Main.java
@@ -0,0 +1,106 @@
+package org.epzilla.dispatcher;
+
+import net.epzilla.stratification.restruct.RestructuringDaemon;
+import org.epzilla.dispatcher.controlers.DispatcherUIController;
+import org.epzilla.dispatcher.controlers.MainDispatcherController;
+import org.epzilla.dispatcher.dataManager.NodeVariables;
+import org.epzilla.dispatcher.loadAnalyzer.CpuMemAnalyzer;
+import org.epzilla.dispatcher.rmi.DispImpl;
+import org.epzilla.dispatcher.rmi.DispInterface;
+import org.epzilla.dispatcher.rmi.DispatcherService;
+import org.epzilla.dispatcher.rmi.OpenSecurityManager;
+import org.epzilla.leader.LeaderElectionInitiator;
+import org.epzilla.util.Logger;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: chathura
+ * Date: Jun 16, 2010
+ * Time: 5:36:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    private static String serviceName = "DISPATCHER_SERVICE";
+    private static String STMserverIP = "127.0.0.1";
+
+    private void bindDispatcher(String serviceName) throws RemoteException, UnknownHostException, MalformedURLException {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new OpenSecurityManager());
+        }
+        DispInterface dispInt = new DispImpl();
+        InetAddress inetAddress;
+        inetAddress = InetAddress.getLocalHost();
+
+        String ipAddress = inetAddress.getHostAddress();
+        NodeVariables.setNodeIP(ipAddress);
+        String id = dispIdGen(ipAddress);
+        String disServiceName = serviceName + id;
+        String name = "rmi://" + ipAddress + "/" + disServiceName;
+        Naming.rebind(name, dispInt);
+    }
+
+    /*
+      * generate dispatcher id
+      */
+
+    private String dispIdGen(String addr) {
+        String[] addrArray = addr.split("\\.");
+        String temp = "";
+        String value = "";
+        for (int i = 0; i < addrArray.length; i++) {
+            temp = addrArray[i];
+            while (temp.length() != 3) {
+                temp = '0' + temp;
+            }
+            value += temp;
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Main main = new Main();
+            main.bindDispatcher(serviceName);
+
+            //added leader election init
+            LeaderElectionInitiator.mainMethod();
+
+
+//        LeaderElectionInitiator.mainMethod();
+            String leader = "";
+            while (leader.equalsIgnoreCase("")) {
+                leader = LeaderElectionInitiator.getLeader();
+
+            }
+            if (leader.equalsIgnoreCase(NodeVariables.getNodeIP())) {
+
+                //To run as Dispatcher as STM server
+                DispatcherUIController.InitializeUI();
+                MainDispatcherController.runAsServer();
+                Logger.log("running as server...");
+                RestructuringDaemon.start();
+
+            } else {
+                DispatcherUIController.InitializeUI();
+                NodeVariables.setCurrentServerIP(leader);
+                MainDispatcherController.runAsClient();
+            }
+
+
+            CpuMemAnalyzer.Initialize();
+            //To run dispatcher as STM client
+            //MainDispatcherController.runAsClient();
+            //Logger.log("running as client...");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
