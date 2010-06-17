b3f1c7cdce3c6f21b4157373e066dc1bb58c7587
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-17 17:20:58 +0000

    updated: add recovered triggers to the STM

--- src-1.2.1-branch/src/org/epzilla/dispatcher/dataManager/RecoveredTriggers.java
+++ src-1.2.1-branch/src/org/epzilla/dispatcher/dataManager/RecoveredTriggers.java
@@ -1,5 +1,7 @@
 package org.epzilla.dispatcher.dataManager;
 
+import org.epzilla.dispatcher.rmi.DispImpl;
+
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -11,13 +13,19 @@ import java.util.StringTokenizer;
  * To change this template use File | Settings | File Templates.
  */
 public class RecoveredTriggers {
+    private static ArrayList<String> tList = new ArrayList<String>();
+
     public RecoveredTriggers() {
 
     }
-
+    /*
+    * method accept arrayList of trigger information received from the Log based recovery
+    * trigger list is split and get the target triggers and the relevant client id
+     * recovered triggers are added to the TriggerManager class to distribute to the Clusters 
+     */
     public static void getRecTriggerList(ArrayList<String> triggers) {
 
-        String trigger,clientID,clusterID;
+        String trigger="", clientID="", clusterID="";
         try {
             for (String tr : triggers) {
                 StringTokenizer st = new StringTokenizer(tr, ":");
@@ -25,8 +33,10 @@ public class RecoveredTriggers {
                 clientID = st.nextToken(); //client ID
                 clusterID = st.nextToken();//cluster id
                 sendTriggers(trigger, clientID, clusterID);
+                tList.add(trigger);
                 System.out.println("Trigger: " + trigger + " Client: " + clientID + " Cluster: " + clusterID);
             }
+            TriggerManager.addAllTriggersToList(tList, clientID);
         } catch (Exception e) {
             e.printStackTrace();
         }
