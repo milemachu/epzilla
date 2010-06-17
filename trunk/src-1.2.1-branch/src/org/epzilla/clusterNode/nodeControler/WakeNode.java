e49d1d00390ed0ffe0c7cb6b608ce5fbcf563113
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-17 06:41:24 +0000

    waking node logic added

--- trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/nodeControler/WakeNode.java
+++ trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/nodeControler/WakeNode.java
@@ -1,11 +1,15 @@
 package org.epzilla.clusterNode.nodeControler;
 
+import org.epzilla.clusterNode.NodeController;
 import org.epzilla.clusterNode.rmi.ClusterInterface;
+import org.epzilla.leader.LeaderElectionInitiator;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,7 +18,29 @@ import java.rmi.RemoteException;
  * Time: 1:45:50 PM
  * To change this template use File | Settings | File Templates.
  */
-public class WakeNode { 
+public class WakeNode {
+    private static HashSet<String> nodeList;
+
+    public static void wake() {
+        if (getNodeDetails()) {
+            String leaderIP = NodeController.getLeaderIP();
+            for (Iterator i = nodeList.iterator(); i.hasNext();) {
+                String ip = (String) i.next();
+                if (!ip.equalsIgnoreCase(leaderIP)) {
+                    nodeInit(ip);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean getNodeDetails() {
+        nodeList.clear();
+        nodeList = LeaderElectionInitiator.getNodes();
+        if (nodeList.size() > 2)
+            return true;
+        return false;
+    }
 
     public static void nodeInit(String nodeIP) {
         try {
