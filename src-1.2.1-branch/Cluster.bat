e08bebccde82a9bb1e6596196ca10954135df255
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-17 14:01:27 +0000

    Changed the jar package name of the solution from DispatcherJSTM to epZilla. Modified all the bat files and pom.xml.

--- src-1.2.1-branch/Cluster.bat
+++ src-1.2.1-branch/Cluster.bat
@@ -1,4 +1,4 @@
 @echo on
 start rmiregistry
-java -classpath %CD%\target\DispatcherJSTM-1.0.0.jar;%CD%\lib\jstm.jar;%CD%\lib\javasysmon-0.3.1.jar; -Djava.security.policy=%CD%\policy.txt -Djava.rmi.server.codebase=file:%CD%\target\DispatcherJSTM-1.0.0.jar org.epzilla.clusterNode.leaderReg.Main
+java -classpath %CD%\target\epZilla-1.0.0.jar;%CD%\lib\jstm.jar;%CD%\lib\javasysmon-0.3.1.jar; -Djava.security.policy=%CD%\policy.txt -Djava.rmi.server.codebase=file:%CD%\target\epZilla-1.0.0.jar org.epzilla.clusterNode.leaderReg.Main
 pause
