6754581528f29f9207927473e401d7a1b2a0de78
dishansm <dishansm@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-17 06:31:58 +0000

    Updated to get Load balancing parameters

--- src-1.2.1-branch/src/org/epzilla/clusterNode/xml/ClusterSettingsReader.java
+++ src-1.2.1-branch/src/org/epzilla/clusterNode/xml/ClusterSettingsReader.java
@@ -2,6 +2,7 @@ package org.epzilla.clusterNode.xml;
 
 import org.epzilla.dispatcher.xml.XMLElement;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -15,27 +16,33 @@ import java.io.FileReader;
  * To change this template use File | Settings | File Templates.
  */
 public class ClusterSettingsReader {
-    public ArrayList<String[]> getServerIPSettings(String filename) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        String line = null;
-        StringBuilder sb = new StringBuilder("");
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
-        }
-        XMLElement xe = new XMLElement();
-        xe.parseString(sb.toString());
-
+    public static ArrayList<String[]> getServerIPSettings(String filename) {
         ArrayList<String[]> lis = new ArrayList<String[]>();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filename));
 
-        String[] items = new String[3];
+            String line = null;
+            StringBuilder sb = new StringBuilder("");
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            XMLElement xe = new XMLElement();
+            xe.parseString(sb.toString());
 
-        for (XMLElement child : xe.getChildren()) {
-            items = new String[2];
-            items[0] = child.getAttribute("id");
-            items[1] = child.getAttribute("nodeStatus");
-            lis.add(items);
+
+            String[] items = new String[4];
+
+            for (XMLElement child : xe.getChildren()) {
+                items[0] = child.getAttribute("mesurmentInterval");
+                items[1] = child.getAttribute("cpuUpperThreashHold");
+                items[2] = child.getAttribute("memUpperThreashHold");
+                items[3] = child.getAttribute("cpuLowerThreashHold");
+                lis.add(items);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
         return lis;
     }
 
