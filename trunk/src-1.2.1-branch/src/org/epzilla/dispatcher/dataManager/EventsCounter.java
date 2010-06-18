c72c594782a9f4b19ea9378473a36a9333010e8b
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-18 19:27:02 +0000

    updated

--- trunk/src-1.2.1-branch/src/org/epzilla/dispatcher/dataManager/EventsCounter.java
+++ trunk/src-1.2.1-branch/src/org/epzilla/dispatcher/dataManager/EventsCounter.java
@@ -4,8 +4,6 @@ import org.epzilla.dispatcher.controlers.DispatcherUIController;
 
 import java.util.TimerTask;
 
-import jstm.transports.clientserver.socket.SocketClient;
-
 /**
  * Created by IntelliJ IDEA.
  * User: chathura
@@ -17,21 +15,36 @@ public class EventsCounter {
 
     public static int countIn = 0;
     public static int countOut = 0;
-    public static int lastOut =0;
+    public static int lastOut = 0;
     static TimerTaskC timer = new TimerTaskC();
+    public static int dispatchRate;
+    public static int maxDispatchRate=1000;
 
     public EventsCounter() {
     }
 
     public static void setInEventCount() {
         countIn++;
-
     }
 
     public static void setOutEventCount() {
         countOut++;
+    }
 
+    public static void setEventDispatchRate(int rate) {
+        dispatchRate = rate;
+    }
 
+    public static int getEventDispatchRate() {
+        return dispatchRate;
+    }
+
+    public static void setMaxRate(int mRate) {
+        maxDispatchRate = mRate;
+    }
+
+    public static int getMaxRate() {
+        return maxDispatchRate;
     }
 }
 
@@ -42,7 +55,19 @@ class TimerTaskC {
         timer1.schedule(new TimerTask() {
             @Override
             public void run() {
-                String textRate = Integer.toString((EventsCounter.countOut - EventsCounter.lastOut)/10);
+                int rate = (EventsCounter.countOut - EventsCounter.lastOut) / 10;
+                if(rate>EventsCounter.getMaxRate()) {
+                    EventsCounter.setMaxRate(rate);
+                }
+                String textRate;
+//                if (rate < 100) {
+//                    textRate = "100";
+//                    rate = 100;
+//                } else {
+//                    textRate = Integer.toString(rate);
+//                }
+                textRate = Integer.toString(rate);
+                EventsCounter.setEventDispatchRate(rate);
                 DispatcherUIController.setEDRate(textRate);
                 String text = Integer.toString(EventsCounter.countOut);
                 DispatcherUIController.appendOutEventCount(text);
