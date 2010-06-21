f0fc273b72c3d076c35799027997180d3f50626a
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-21 18:38:21 +0000

    Adding security for nodes

--- trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/node/TCPMessageDecoder.java
+++ trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/node/TCPMessageDecoder.java
@@ -1,6 +1,9 @@
 package org.epzilla.common.discovery.node;
 
+import java.util.HashSet;
+
 import org.epzilla.common.discovery.Constants;
+import org.epzilla.leader.Epzilla;
 
 public class TCPMessageDecoder implements Runnable {
 
@@ -8,6 +11,7 @@ public class TCPMessageDecoder implements Runnable {
     private static String LEADER_SERVICE_NAME= "LEADER_SERVICE";
     private static String SUBSCRIBE_PREFIX = "SUBSCRIBE_";
 	private static String UNSUBSCRIBE_PREFIX = "UNSUBSCRIBE_";
+	private HashSet<String> authorizedNodeList=new HashSet<String>(Epzilla.getComponentIpList().values());
 
 	public TCPMessageDecoder(String message) {
 		this.message=message;
@@ -16,11 +20,11 @@ public class TCPMessageDecoder implements Runnable {
 	public void run() {
 		//0=message,1=ip
 		String []tcpArr=message.split(Constants.TCP_UNICAST_DELIMITER);
-		if(tcpArr[0].equalsIgnoreCase(SUBSCRIBE_PREFIX+LEADER_SERVICE_NAME) && NodeDiscoveryManager.isLeader() /*&&  !NodeDiscoveryManager.getLeaderPublisher().getSubscribers().contains(tcpArr[1])*/){
+		if(tcpArr[0].equalsIgnoreCase(SUBSCRIBE_PREFIX+LEADER_SERVICE_NAME) && NodeDiscoveryManager.isLeader() &&  authorizedNodeList.contains(tcpArr[1])){
 			
 				NodeDiscoveryManager.getLeaderPublisher().addSubscription(tcpArr[1], tcpArr[0]);
 			
-		}else if(tcpArr[0].equalsIgnoreCase(UNSUBSCRIBE_PREFIX+LEADER_SERVICE_NAME) && NodeDiscoveryManager.isLeader()){
+		}else if(tcpArr[0].equalsIgnoreCase(UNSUBSCRIBE_PREFIX+LEADER_SERVICE_NAME) && NodeDiscoveryManager.isLeader() &&  authorizedNodeList.contains(tcpArr[1])){
 			NodeDiscoveryManager.getLeaderPublisher().removeSubscrition(tcpArr[1], tcpArr[0]);
 		}
 	}
