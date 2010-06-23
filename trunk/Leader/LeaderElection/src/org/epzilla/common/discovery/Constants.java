b0fad27f777ddf4673a269cac1235b6ee008c2eb
harshana05 <harshana05@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-06-23 18:14:05 +0000

    Comments added.

--- trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/Constants.java
+++ trunk/Leader/LeaderElection/src/org/epzilla/common/discovery/Constants.java
@@ -1,12 +1,34 @@
 package org.epzilla.common.discovery;
 
+/**
+ * This class contains the constant which are using in the dynamic service discovery component.
+ * @author Administrator
+ *
+ */
 public interface Constants {
-	public static String MULTICAST_DELIMITER="\u0001";	
+	/*
+	 * Multicast message delimiter uses in the component 
+	 */
+	public static String MULTICAST_DELIMITER="\u0001";
+	
+	/*
+	 * TCP message delimiter uses in the component
+	 */
 	public static String TCP_UNICAST_DELIMITER="\u0002";
 	
+	/*
+	 * Node leaders uses this delimiter when sending messages to dispatchers.
+	 */
 	public static String DISPATCHER_CLIENT_DELIMITER="\u0005";
+	
+	/*
+	 * Node uses this delimiter when sending messages to node cluster leaders.
+	 */
 	public static String NODE_CLIENT_DELIMITER="\u0006";
 	
+	/*
+	 * This is used to separate the cluster Id.
+	 */
 	public static String CLUSTER_ID_DELIMITER="\u0010";
 
 }
