282e4d0fbc2f0b15d785d2e52c7cf1855268918c
chathuraran <chathuraran@6d78cf82-acc7-11de-ba6e-69e41c8db662>
2010-07-17 12:24:41 +0000

    Class to set the configuration data of EpZilla IP config file

---
+++ trunk/src-1.2.1-branch/src/org/epzilla/clusterNode/xml/ConfigurationManager.java
@@ -0,0 +1,18 @@
+package org.epzilla.clusterNode.xml;
+
+/**
+ * Created by IntelliJ IDEA.
+ * This class is to modify the configuration settings in the XML file
+ * Author: Chathura
+ * Date: Jul 17, 2010
+ * Time: 5:11:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfigurationManager {
+    /**
+     * write configuration data to the file
+     */
+    public static void writeData(String startIp, String endIp){
+
+    }
+}
